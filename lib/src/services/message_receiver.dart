1e3c9a084829cfe20380d36dc882d637e892932b
Levi Lesches <levilesches@gmail.com>
2022-07-22 05:09:24 -0400

    Implement the `MessageSender` and `MessageReceiver` services.

---
+++ lib/src/services/message_receiver.dart
@@ -0,0 +1,78 @@
+import "dart:async";
+import "dart:io";
+
+import "package:rover_dashboard/data.dart";
+import "service.dart";
+
+/// A callback to execute with raw Protobuf data.
+typedef RawMessageHandler = void Function(List<int> data);
+
+/// A callback to execute with a specific serialized Protobuf message.
+typedef MessageHandler<T extends Message> = void Function(T);
+
+/// The port to listen for messages on.
+const port = 22201;
+
+/// A service that receives messages over a UDP connection. 
+/// 
+/// To listen to certain messages, call [registerHandler] with the type of message you want
+/// to receive, as well as a decoder and the handler callback itself. 
+class MessageReceiver extends Service {
+	/// Handlers for every possible type of Protobuf message in serialized form.
+	final Map<String, RawMessageHandler> _handlers = {};
+
+	/// The UDP socket to listen on.
+	/// 
+	/// Initialized in [init].
+	late final RawDatagramSocket _socket;
+
+	/// The subscription that listens to [_socket]. 
+	late final StreamSubscription _subscription;
+
+	@override
+	Future<void> init() async {
+		_socket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, port);
+		_subscription = _socket.listen(_listener);
+	}
+
+	@override
+	Future<void> dispose() async {
+		await _subscription.cancel();
+		_socket.close();
+	}
+
+	/// Runs every time data is received by the socket. 
+	/// 
+	/// The datagram contains a [WrappedMessage]. These are Protobuf messages that wrap an
+	/// underlying message and record their name. We use the type of the underlying message
+	/// to get the appropriate handler from [_handlers] which decodes the message to the 
+	/// correct type and processes it. 
+	void _listener(RawSocketEvent event) {
+		final Datagram? datagram = _socket.receive();
+		if (datagram == null) return;
+
+		final data = datagram.data;
+		final wrapped = WrappedMessage.fromBuffer(data);
+		final RawMessageHandler? rawHandler = _handlers[wrapped.name];
+		if (rawHandler == null) { /* Log in some meaningful way, through the UI */ }
+		else { rawHandler(wrapped.data); }
+	}
+
+	/// Adds a handler for a given type. 
+	/// 
+	/// [decoder] is a function that decodes a byte buffer to a Protobuf message class. [handler] 
+	/// then handles that message somehow. 
+	void registerHandler<T extends Message>({
+		required String name, 
+		required MessageDecoder<T> decoder, 
+		required MessageHandler<T> handler
+	}) {
+		if (T == Message) {  // no T was actually passed, [Message] is the default
+			throw ArgumentError("No message type was passed");
+		} else if (_handlers.containsKey(name)) {  // handler was already registered
+			throw ArgumentError("Message handler for type [$T] already registered");
+		} else {
+			_handlers[name] = (List<int> data) => handler(decoder(data));
+		}
+	}
+}
---
+++ lib/src/services/message_sender.dart
@@ -0,0 +1,41 @@
+import "dart:io";
+
+import "package:rover_dashboard/data.dart";
+
+import "service.dart";
+
+/// A service to send [Message] objects to the rover. 
+class MessageSender extends Service {
+	/// The IP address of the rover. 
+	static final address = InternetAddress("127.0.0.1");
+
+	/// The port on the rover to send to. 
+	static const port = 8082;
+
+	/// The socket for the UDP connection. 
+	late final RawDatagramSocket _socket;
+
+	@override
+	Future<void> init() async {
+		_socket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, port);
+	}
+
+	@override
+	Future<void> dispose() async { 
+		_socket.close();
+	}
+
+	/// Resets the connection. 
+	/// 
+	/// When in doubt, turn it off and back on again.
+	Future<void> reset() async {
+		await dispose();
+		await init();
+	} 
+
+	/// Wraps the [message] in a [WrappedMessage] container and sends it to the rover. 
+	Future<void> sendMessage(Message message) async { 
+		final wrapper = WrappedMessage(message.info_.messageName, message.writeToBuffer());
+		_socket.send(wrapper.writeToBuffer(), address, port);
+	}
+}
