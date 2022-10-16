dcb31ea5fae391251c7db12886766bd9e37c6a10
AndyZ54 <Andyzheng54@yahoo.com>
2022-10-16 15:57:04 -0400

    Made electrical message

--- lib/src/data/metrics.dart
+++ lib/src/data/metrics.dart
@@ -29,22 +29,22 @@ abstract class Metrics<T extends GeneratedMessage> {
 /// These metrics represent the vitals of the rover: basics like voltage, current, and temperature
 /// of the various electrical components. These values aren't useful for the missions, but should
 /// be monitored to catch problems before they cause damage to the rover. 
-// class ElectricalMetrics extends Metrics {
-// 	/// A collection of metrics relevant for monitoring the rover's electrical status.
-// 	const ElectricalMetrics(super.data);
+class ElectricalMetrics extends Metrics<Electrical> {
+	/// A collection of metrics relevant for monitoring the rover's electrical status.
+	const ElectricalMetrics(super.data);
 
-// 	@override
-// 	String get name => "Electrical";
+	@override
+	String get name => "Electrical";
 
-// 	// TODO: implement this
-// 	@override
-// 	List<String> get allMetrics => [  
-// 		"Battery: ${data.batteryVoltage} V, ${data.batteryCurrent} A",
-// 		"12V supply: ${data.v12SupplyVoltage} V, ${data.v12SupplyCurrent} A, ${data.v12SupplyTemperature} °F",
-// 		"5V supply: ${data.v5SupplyVoltage} V, ${data.v5SupplyCurrent} A, ${data.v5SupplyTemperature} °F",
-// 		"ODrives: ${data.odriveCurrent1} A, ${data.odriveCurrent2} A, ${data.odriveCurrent3} A",
-// 	];
-// }
+	// TODO: implement this
+	@override
+	List<String> get allMetrics => [  
+		"Battery: ${data.batteryVoltage} V, ${data.batteryCurrent} A",
+		"12V supply: ${data.v12SupplyVoltage} V, ${data.v12SupplyCurrent} A, ${data.v12SupplyTemperature} °F",
+		"5V supply: ${data.v5SupplyVoltage} V, ${data.v5SupplyCurrent} A, ${data.v5SupplyTemperature} °F",
+		"ODrives: ${data.odrive0Current} A, ${data.odrive1Current} A, ${data.odrive2Current} A",
+	];
+}
 
 /// Metrics reported by the science subsystem. 
 /// 
