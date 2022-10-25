022915295a2032bedce240dbddab997cab6853f1
Levi Lesches <levilesches@gmail.com>
2022-10-25 03:27:11 -0400

    Some improvements to Protobuf handling

--- lib/src/data/metrics.dart
+++ lib/src/data/metrics.dart
@@ -29,7 +29,7 @@ abstract class Metrics<T extends GeneratedMessage> {
 /// These metrics represent the vitals of the rover: basics like voltage, current, and temperature
 /// of the various electrical components. These values aren't useful for the missions, but should
 /// be monitored to catch problems before they cause damage to the rover. 
-class ElectricalMetrics extends Metrics<Electrical> {
+class ElectricalMetrics extends Metrics<ElectricalData> {
 	/// A collection of metrics relevant for monitoring the rover's electrical status.
 	const ElectricalMetrics(super.data);
 
