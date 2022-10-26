3facb104388e1fc7e47e7d0e99b0fcb03945ca1b
Levi Lesches <levilesches@gmail.com>
2022-10-26 01:11:57 -0400

    Proto (#27)
    - Added all generated Protobuf files to the repository
    
    This is bad practice in general, but
    1) Our project is small enough that it won't result in too much bloat
    2) Protobuf has been reduced to `dart run build_runner build`, which doesn't
       teach anything about Protobuf to be a "useful skill"
    3) Any changes to Protobuf files require _everyone_ to pull the submodule and
       regenerate the files, without a clear error telling them so. This way, when
       one person changes the Protobuf files and tests it, they'll also regenerate
       the code and push those new versions to the repo.
    
    - Added two scripts to test networking without booting up Flutter

---
+++ bin/receive.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: avoid_print
+
+import "package:rover_dashboard/data.dart";
+import "package:rover_dashboard/services.dart";
+
+void electricalHandler(ElectricalData data) => print("The battery is ${data.batteryVoltage}V");
+
+Future<void> main() async {
+	final receiver = MessageReceiver();
+	await receiver.init();
+	print("Listening on $port");
+
+	receiver.registerHandler<ElectricalData>(
+		name: "ElectricalData",
+		decoder: ElectricalData.fromBuffer,
+		handler: electricalHandler,
+	);
+}
