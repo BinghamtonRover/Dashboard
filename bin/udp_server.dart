26279cf7fd9fdaf0b11fca67cb08a5b88d4b3c21
Levi Lesches <levilesches@gmail.com>
2022-12-29 14:06:09 -0500

    Added new test for UDP server

---
+++ bin/udp_server.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: avoid_print
+
+import "package:rover_dashboard/services.dart";
+
+const int receivePort = 8001;
+const int sendPort = 8002;
+
+class RawServer extends UdpServer {
+	RawServer() : super(port: receivePort);
+
+	@override
+	void onData(List<int> bytes) => print("Received: $bytes");
+}
+
+void main() async {
+	final server = RawServer();
+	await server.init();
+	print("Listening on :$receivePort");
+}
