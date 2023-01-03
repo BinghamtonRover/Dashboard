6481331d2019a4dfb495e1541b850631ba07bb97
Levi Lesches <levilesches@gmail.com>
2023-01-03 15:38:17 -0500

    Documented Serial and modified it to use new SerialExceptions

--- lib/src/services/serial.dart
+++ lib/src/services/serial.dart
@@ -1,84 +1,107 @@
 import "dart:typed_data";
 
 import "package:libserialport/libserialport.dart";
+
+import "serial_errors.dart";
 import "service.dart";
 
-/// Exception for Serial class when there are errors connecting to device
-class DeviceNotConnected implements Exception {
-  /// Reason for exception
-  String cause;
-  /// Constructor for class which takes in error message
-  DeviceNotConnected(this.cause);
-
-  @override
-  String toString() => cause;
-}
-
-
-
-
-/// Service to interact with Serial Port(s) from dashboard
+/// A service to interact with Serial Port(s) from dashboard.
+/// 
+/// Since a device may not be connected right away, or may be disconnected after the dashboard is
+/// running, this service needs to explicitly connect to a device using [connect]. The device may
+/// be disconnected by calling [disconnect]. In any event, [dispose] will also call [disconnect].
 class Serial extends Service {
-  /// object to send data with
-  SerialPort? writer;
-  /// object to read data from
-  SerialPortReader? reader;
+	/// Writes data to the serial device. 
+	/// 
+	/// This field is only non-null after calling [connect] successfully.
+	SerialPort? _writer;
 
-  /// Initialize the port and open for read/write
-  @override
-  Future<void> init() async {}
+	/// Reads data from the serial device.
+	/// 
+	/// This field is only non-null after calling [connect] successfully.
+	SerialPortReader? _reader;
 
-  /// Close the port 
-  @override
-  Future<void> dispose() async {
-    writer?.dispose();
-  }
+	@override
+	Future<void> init() async {}
 
-  /// Initialize the port and open for read/write
-  Future<void> connect() async {
-    if (SerialPort.availablePorts.isEmpty) {
-      throw DeviceNotConnected("No Device available to be connected to"); 
-    }
-    final name = SerialPort.availablePorts.first;
-    writer = SerialPort(name);
-    reader = SerialPortReader(writer!);
-  }
+	@override
+	Future<void> dispose() async {
+		_writer?.dispose();
+		_reader?.close();
+	}
 
-  /// Write some data
-  void sendBytes(String data) {
-    final List<int> codeUnits = data.codeUnits;
-    final Uint8List byteList = Uint8List.fromList(codeUnits);
-    if(writer == null) throw DeviceNotConnected("Serial Port Writer is not connected");
-    writer!.write(Uint8List.fromList(byteList), timeout: 500);
-  }
+	/// Connects a device to the dashboard for reading and writing.
+	/// 
+	/// - If no devices are found, this will throw [NoDeviceFound].
+	/// - If multiple devices are found, this will throw [MultipleDevicesFound].
+	Future<void> connect() async {
+		final List<String> devices = SerialPort.availablePorts;
+		if (devices.isEmpty) { throw NoDeviceFound(); }
+		else if (devices.length > 1) { throw MultipleDevicesFound(devices); }
+		disconnect();  // only when exactly one new device is found
+		final name = devices.first;
+		_writer = SerialPort(name);
+		_reader = SerialPortReader(_writer!);
+	}
 
-  /// Verify that the command is the correct format
-  /// It must start with ":" and end with "\n" ex: :command arg1 arg2\n
-  /// Otherwise throw a format exception
-  String parseCommand(Uint8List packet) {
-    final buffer = StringBuffer();
-    final String data = String.fromCharCodes(packet);
-    for (int i = 0; i < data.length; i++) {
-      final String char = data[i];
-      if (buffer.isEmpty) {
-        if (char != ":") throw const FormatException("Command should start with :");
-      } else {
-        if (char == ":") throw const FormatException("Command cannot contain a :");
-      }
-      buffer.write(char);
-      if (char == "\n") {
-        if (i != data.length - 1){
-          throw const FormatException("Command cannot contain a \\n");
-        }
-          return buffer.toString().substring(1);
-        }
-    }
-    throw const FormatException("Command did not end with \n");
-  }
+	/// Disposes of the current [_reader] and [_writer] and marks them as null.
+	/// 
+	/// Use this to indicate that the previous device is no longer available.
+	void disconnect() {
+		dispose();
+		_writer = null;
+		_reader = null;
+	}
 
-  /// Returns a stream of all commands being sent
-  Stream<String> incomingData() {
-    if(reader == null) throw DeviceNotConnected("Serial Port Reader is not connected");
-    return reader!.stream.map(parseCommand);
-  }
+	/// Write data to the device.
+	/// 
+	/// - If no device is connected, this will throw a [DeviceNotConnected].
+	/// - If there is an IO error, this will throw a [SerialPortError].
+	void sendBytes(String data) {
+		final Uint8List byteList = Uint8List.fromList(data.codeUnits);
+		if (_writer == null) throw DeviceNotConnected();
+		try {
+			_writer!.write(Uint8List.fromList(byteList), timeout: 500);
+		} on SerialPortError catch (error) {
+			disconnect();
+			throw SerialIOError(error);
+		}
+	}
+
+	/// Verify that the command is the correct format
+	/// It must start with ":" and end with "\n" ex: :command arg1 arg2\n
+	/// Otherwise throw a format exception
+	String _parseCommand(Uint8List packet) {
+		final buffer = StringBuffer();
+		final String data = String.fromCharCodes(packet);
+		for (int i = 0; i < data.length; i++) {
+			final String char = data[i];
+			if (buffer.isEmpty) {
+				if (char != ":") throw MalformedSerialPacket(packet: data, reason: "Command should start with :");
+			} else {
+				if (char == ":") throw MalformedSerialPacket(packet: data, reason: "Command cannot contain a :");
+			}
+			buffer.write(char);
+			if (char == "\n") {
+				if (i != data.length - 1){
+					throw MalformedSerialPacket(packet: data, reason: "Command cannot contain a \\n");
+				}
+					return buffer.toString().substring(1);
+				}
+		}
+		throw MalformedSerialPacket(packet: data, reason: "Command did not end with \n");
+	}
+
+	/// Returns a stream of all commands being sent.
+	/// 
+	/// - If no device is connected, this will throw a [DeviceNotConnected].
+	/// - If there is an IO error, this will throw a [SerialPortError].
+	Stream<String> incomingData() {
+		if (_reader == null) throw DeviceNotConnected();
+		try { return _reader!.stream.map(_parseCommand); }
+		on SerialPortError catch (error) {
+			disconnect();
+			throw SerialIOError(error);
+		}
+	}
 }
