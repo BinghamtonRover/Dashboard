aa6ae5fc78e6eb5f7931f185d31fc9e132ac9110
aidanahram <aidanahram1@gmail.com>
2023-01-24 15:30:21 -0500

    added classes for science and automation

--- lib/src/services/control/drive.dart
+++ lib/src/services/control/drive.dart
@@ -1,9 +1,14 @@
+import "../message_sender.dart";
 import "../service.dart";
+
 
 /// A service to send commands to the Rover when it is the [DriveControl] mode
 class DriveControl extends Service {
+  
   @override
-  Future<void> init() async {} 
+  Future<void> init() async {
+    final sender = MessageSender(); 
+  } 
 
   @override
 	Future<void> dispose() async {}
@@ -11,11 +16,12 @@ class DriveControl extends Service {
   /// Command to change the Rover's speed
   void updateSpeed(double left, double right, double throttle) { 
     //final message = DriveCommand(left, right, throttle);  // TODO: Protobuf class
-    //services.messageSender.send_message(message);
+    //sender.send_message(message);
   }
   
   /// Command to tell the Rover to brake
   void brake(){
-
+    //final message = DriveCommand();
+    //sender.send_message(message);
   }
 }
