19e280fd27b9433fddc1bf18ac465fce0deb317d
Levi Lesches <levilesches@gmail.com>
2023-02-08 23:52:06 -0500

    Added a handshake to Serial

--- lib/src/services/serial.dart
+++ lib/src/services/serial.dart
@@ -1,6 +1,9 @@
 import "dart:typed_data";
 
 import "package:flutter_libserialport/flutter_libserialport.dart";
+import "package:protobuf/protobuf.dart";
+
+import "package:rover_dashboard/data.dart";
 
 import "serial_errors.dart";
 import "service.dart";
@@ -32,75 +35,76 @@ class Serial extends Service {
 		_reader?.close();
 	}
 
+	/// All the Serial ports available to connect to.
+	/// 
+	/// Not all of these will be for the firmware, so the user needs to choose the right one. To make
+	/// the display cleaner, this collection filters out duplicate entries.
+	Iterable<String> get availablePorts => SerialPort.availablePorts.toSet();
+
 	/// Connects a device to the dashboard for reading and writing.
 	/// 
-	/// - If no devices are found, this will throw [NoDeviceFound].
-	/// - If multiple devices are found, this will throw [MultipleDevicesFound].
-	Future<void> connect() async {
-		final List<String> devices = SerialPort.availablePorts;
-		if (devices.isEmpty) { throw NoDeviceFound(); }
-		else if (devices.length > 1) { throw MultipleDevicesFound(devices); }
-		disconnect();  // only when exactly one new device is found
-		final name = devices.first;
-		_writer = SerialPort(name);
+	/// Sends a [Connect] message, then expects a [Connect] message in response.
+	Future<void> connect(String device) async {
+		await disconnect();  // only when exactly one new device is found
+		_writer = SerialPort(device);
 		_reader = SerialPortReader(_writer!);
+		final didOpen = _writer!.openReadWrite();
+		if (!didOpen) {
+			await disconnect();
+			throw SerialCannotOpen(device);
+		}
+
+		// Send a handshake, expect one back
+		await sendMessage(Connect(sender: Device.DASHBOARD, receiver: Device.FIRMWARE));
+		await Future.delayed(const Duration(seconds: 1));
+		final received = _writer!.read(4);  // nice way to read X bytes at a time
+		try {
+			final message = Connect.fromBuffer(received);
+			final isValid = message.sender == Device.FIRMWARE && message.receiver == Device.DASHBOARD;
+			if (!isValid) {
+				await disconnect();
+				throw SerialHandshakeFailed();
+			}
+		} on InvalidProtocolBufferException {
+			await disconnect();
+			throw MalformedSerialPacket(packet: received);
+		}
 	}
 
 	/// Disposes of the current [_reader] and [_writer] and marks them as null.
 	/// 
 	/// Use this to indicate that the previous device is no longer available.
-	void disconnect() {
-		dispose();
+	Future<void> disconnect() async {
+		await dispose();
 		_writer = null;
 		_reader = null;
 	}
+
+	/// Sends a [Message] over Serial, without wrapping it.
+	Future<void> sendMessage(Message message) => 
+		sendRaw(message.writeToBuffer());
 
 	/// Write data to the device.
 	/// 
 	/// - If no device is connected, this will throw a [DeviceNotConnected].
 	/// - If there is an IO error, this will throw a [SerialPortError].
-	void send(String data) {
-		final Uint8List byteList = Uint8List.fromList(data.codeUnits);
+	Future<void> sendRaw(Uint8List data) async {
 		if (_writer == null) throw DeviceNotConnected();
 		try {
-			_writer!.write(Uint8List.fromList(byteList), timeout: 500);
+			_writer!.write(Uint8List.fromList(data), timeout: 500);
 		} on SerialPortError catch (error) {
-			disconnect();
+			await disconnect();
 			throw SerialIOError(error);
 		}
-	}
-
-	/// Verify that the command is the correct format
-	/// It must start with ":" and end with "\n" ex: :command arg1 arg2\n
-	/// Otherwise throw a format exception
-	String _parseCommand(Uint8List packet) {
-		final buffer = StringBuffer();
-		final String data = String.fromCharCodes(packet);
-		for (int i = 0; i < data.length; i++) {
-			final String char = data[i];
-			if (buffer.isEmpty) {
-				if (char != ":") throw MalformedSerialPacket(packet: data, reason: "Command should start with :");
-			} else {
-				if (char == ":") throw MalformedSerialPacket(packet: data, reason: "Command cannot contain a :");
-			}
-			buffer.write(char);
-			if (char == "\n") {
-				if (i != data.length - 1){
-					throw MalformedSerialPacket(packet: data, reason: "Command cannot contain a \\n");
-				}
-					return buffer.toString().substring(1);
-				}
-		}
-		throw MalformedSerialPacket(packet: data, reason: "Command did not end with \n");
 	}
 
 	/// Returns a stream of all commands being sent.
 	/// 
 	/// - If no device is connected, this will throw a [DeviceNotConnected].
 	/// - If there is an IO error, this will throw a [SerialPortError].
-	Stream<String> incomingData() {
+	Stream<Uint8List> get incomingData {
 		if (_reader == null) throw DeviceNotConnected();
-		try { return _reader!.stream.map(_parseCommand); }
+		try { return _reader!.stream; }
 		on SerialPortError catch (error) {
 			disconnect();
 			throw SerialIOError(error);
