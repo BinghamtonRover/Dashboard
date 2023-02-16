888d099ae1ee1c220c4521fe986a6c18f471b419
Levi Lesches <levilesches@gmail.com>
2023-02-16 02:54:34 -0500

    Improved handshake protocol

--- lib/src/models/rover/rover.dart
+++ lib/src/models/rover/rover.dart
@@ -3,21 +3,28 @@ import "dart:async";
 import "../model.dart";
 import "core.dart";
 
+/// The model to control the entire rover.
+/// 
+/// Find more specific functionality in this class's fields.
 class Rover extends Model {
+	/// Monitors the connection to the rover.
 	final core = RoverCore();
 
-	bool get isConnected => core.isConnected;
+	/// Whether the rover is connected.
+	bool get isConnected => core.connectionStrength > 0;
 
 	@override
 	Future<void> init() async { 
 		await core.init();
+
+		core.addListener(notifyListeners);
 	}
 
 	@override
 	void dispose() {
 		core.dispose();
+
+		core.removeListener(notifyListeners);
 		super.dispose();
 	}
-
-	Future<void> connect() => core.connect();
 }
