34f2ab7a06a570fcb109ad3ef8e895491f4c5d6d
Levi Lesches <levilesches@gmail.com>
2023-03-01 01:58:14 -0500

    Updated `science.proto`

--- lib/src/data/generated/science.pbjson.dart
+++ lib/src/data/generated/science.pbjson.dart
@@ -15,20 +15,21 @@ const ScienceCommand$json = const {
     const {'1': 'dig', '3': 1, '4': 1, '5': 8, '10': 'dig'},
     const {'1': 'spin_carousel_tube', '3': 2, '4': 1, '5': 8, '10': 'spinCarouselTube'},
     const {'1': 'spin_carousel_section', '3': 3, '4': 1, '5': 8, '10': 'spinCarouselSection'},
-    const {'1': 'vacuum_suck', '3': 4, '4': 1, '5': 8, '10': 'vacuumSuck'},
-    const {'1': 'carousel_angle', '3': 5, '4': 1, '5': 2, '10': 'carouselAngle'},
-    const {'1': 'carousel_linear_position', '3': 6, '4': 1, '5': 2, '10': 'carouselLinearPosition'},
-    const {'1': 'test_linear_position', '3': 7, '4': 1, '5': 2, '10': 'testLinearPosition'},
-    const {'1': 'vacuum_linear_position', '3': 8, '4': 1, '5': 2, '10': 'vacuumLinearPosition'},
+    const {'1': 'vacuum_suck', '3': 4, '4': 1, '5': 2, '10': 'vacuumSuck'},
+    const {'1': 'carousel_angle', '3': 5, '4': 1, '5': 5, '10': 'carouselAngle'},
+    const {'1': 'carousel_linear_position', '3': 6, '4': 1, '5': 5, '10': 'carouselLinearPosition'},
+    const {'1': 'test_linear_position', '3': 7, '4': 1, '5': 5, '10': 'testLinearPosition'},
+    const {'1': 'vacuum_linear_position', '3': 8, '4': 1, '5': 5, '10': 'vacuumLinearPosition'},
     const {'1': 'pump1', '3': 9, '4': 1, '5': 8, '10': 'pump1'},
     const {'1': 'pump2', '3': 10, '4': 1, '5': 8, '10': 'pump2'},
     const {'1': 'pump3', '3': 11, '4': 1, '5': 8, '10': 'pump3'},
     const {'1': 'pump4', '3': 12, '4': 1, '5': 8, '10': 'pump4'},
+    const {'1': 'dirtRelease', '3': 13, '4': 1, '5': 5, '10': 'dirtRelease'},
   ],
 };
 
 /// Descriptor for `ScienceCommand`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List scienceCommandDescriptor = $convert.base64Decode('Cg5TY2llbmNlQ29tbWFuZBIQCgNkaWcYASABKAhSA2RpZxIsChJzcGluX2Nhcm91c2VsX3R1YmUYAiABKAhSEHNwaW5DYXJvdXNlbFR1YmUSMgoVc3Bpbl9jYXJvdXNlbF9zZWN0aW9uGAMgASgIUhNzcGluQ2Fyb3VzZWxTZWN0aW9uEh8KC3ZhY3V1bV9zdWNrGAQgASgIUgp2YWN1dW1TdWNrEiUKDmNhcm91c2VsX2FuZ2xlGAUgASgCUg1jYXJvdXNlbEFuZ2xlEjgKGGNhcm91c2VsX2xpbmVhcl9wb3NpdGlvbhgGIAEoAlIWY2Fyb3VzZWxMaW5lYXJQb3NpdGlvbhIwChR0ZXN0X2xpbmVhcl9wb3NpdGlvbhgHIAEoAlISdGVzdExpbmVhclBvc2l0aW9uEjQKFnZhY3V1bV9saW5lYXJfcG9zaXRpb24YCCABKAJSFHZhY3V1bUxpbmVhclBvc2l0aW9uEhQKBXB1bXAxGAkgASgIUgVwdW1wMRIUCgVwdW1wMhgKIAEoCFIFcHVtcDISFAoFcHVtcDMYCyABKAhSBXB1bXAzEhQKBXB1bXA0GAwgASgIUgVwdW1wNA==');
+final $typed_data.Uint8List scienceCommandDescriptor = $convert.base64Decode('Cg5TY2llbmNlQ29tbWFuZBIQCgNkaWcYASABKAhSA2RpZxIsChJzcGluX2Nhcm91c2VsX3R1YmUYAiABKAhSEHNwaW5DYXJvdXNlbFR1YmUSMgoVc3Bpbl9jYXJvdXNlbF9zZWN0aW9uGAMgASgIUhNzcGluQ2Fyb3VzZWxTZWN0aW9uEh8KC3ZhY3V1bV9zdWNrGAQgASgCUgp2YWN1dW1TdWNrEiUKDmNhcm91c2VsX2FuZ2xlGAUgASgFUg1jYXJvdXNlbEFuZ2xlEjgKGGNhcm91c2VsX2xpbmVhcl9wb3NpdGlvbhgGIAEoBVIWY2Fyb3VzZWxMaW5lYXJQb3NpdGlvbhIwChR0ZXN0X2xpbmVhcl9wb3NpdGlvbhgHIAEoBVISdGVzdExpbmVhclBvc2l0aW9uEjQKFnZhY3V1bV9saW5lYXJfcG9zaXRpb24YCCABKAVSFHZhY3V1bUxpbmVhclBvc2l0aW9uEhQKBXB1bXAxGAkgASgIUgVwdW1wMRIUCgVwdW1wMhgKIAEoCFIFcHVtcDISFAoFcHVtcDMYCyABKAhSBXB1bXAzEhQKBXB1bXA0GAwgASgIUgVwdW1wNBIgCgtkaXJ0UmVsZWFzZRgNIAEoBVILZGlydFJlbGVhc2U=');
 @$core.Deprecated('Use scienceDataDescriptor instead')
 const ScienceData$json = const {
   '1': 'ScienceData',
