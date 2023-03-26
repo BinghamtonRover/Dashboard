77e81aab5c7f094aec1bd2543e4087f79d89bbb0
Aidan <aidanahram1@gmail.com>
2023-03-26 16:26:28 -0400

    added some serial.dart functionality

--- lib/src/services/serial.dart
+++ lib/src/services/serial.dart
@@ -11,103 +11,112 @@ import "service.dart";
 export "serial_errors.dart";
 
 /// A service to interact with Serial Port(s) from dashboard.
-/// 
+///
 /// Since a device may not be connected right away, or may be disconnected after the dashboard is
 /// running, this service needs to explicitly connect to a device using [connect]. The device may
 /// be disconnected by calling [disconnect]. In any event, [dispose] will also call [disconnect].
 class Serial extends Service {
-	/// Writes data to the serial device. 
-	/// 
-	/// This field is only non-null after calling [connect] successfully.
-	SerialPort? _writer;
+  /// Writes data to the serial device.
+  ///
+  /// This field is only non-null after calling [connect] successfully.
+  SerialPort? _writer;
 
-	/// Reads data from the serial device.
-	/// 
-	/// This field is only non-null after calling [connect] successfully.
-	SerialPortReader? _reader;
+  /// Reads data from the serial device.
+  ///
+  /// This field is only non-null after calling [connect] successfully.
+  SerialPortReader? _reader;
 
-	@override
-	Future<void> init() async {}
+  /// Device that is currently connected
+  ///
+  /// This field is only non-null after calling [connect] successfully.
+  /// This field gets assigned after revceiving response from the handshake
+  Device? connectedDevice;
 
-	@override
-	Future<void> dispose() async {
-		_writer?.dispose();
-		_reader?.close();
-	}
+  @override
+  Future<void> init() async {}
 
-	/// All the Serial ports available to connect to.
-	/// 
-	/// Not all of these will be for the firmware, so the user needs to choose the right one. To make
-	/// the display cleaner, this collection filters out duplicate entries.
-	Iterable<String> get availablePorts => SerialPort.availablePorts.toSet();
+  @override
+  Future<void> dispose() async {
+    _writer?.dispose();
+    _reader?.close();
+  }
 
-	/// Connects a device to the dashboard for reading and writing.
-	/// 
-	/// Sends a [Connect] message, then expects a [Connect] message in response.
-	Future<void> connect(String device) async {
-		await disconnect();  // only when exactly one new device is found
-		_writer = SerialPort(device);
-		_reader = SerialPortReader(_writer!);
-		final didOpen = _writer!.openReadWrite();
-		if (!didOpen) {
-			await disconnect();
-			throw SerialCannotOpen(device);
-		}
+  /// All the Serial ports available to connect to.
+  ///
+  /// Not all of these will be for the firmware, so the user needs to choose the right one. To make
+  /// the display cleaner, this collection filters out duplicate entries.
+  Iterable<String> get availablePorts => SerialPort.availablePorts.toSet();
 
-		// Send a handshake, expect one back
-		await sendMessage(Connect(sender: Device.DASHBOARD, receiver: Device.FIRMWARE));
-		await Future.delayed(const Duration(seconds: 1));
-		final received = _writer!.read(4);  // nice way to read X bytes at a time
-		try {
-			final message = Connect.fromBuffer(received);
-			final isValid = message.sender == Device.FIRMWARE && message.receiver == Device.DASHBOARD;
-			if (!isValid) {
-				await disconnect();
-				throw SerialHandshakeFailed();
-			}
-		} on InvalidProtocolBufferException {
-			await disconnect();
-			throw MalformedSerialPacket(packet: received);
-		}
-	}
+  /// Connects a device to the dashboard for reading and writing.
+  ///
+  /// Sends a [Connect] message, then expects a [Connect] message in response.
+  Future<void> connect(String device) async {
+    await disconnect(); // only when exactly one new device is found
+    _writer = SerialPort(device);
+    _reader = SerialPortReader(_writer!);
+    final didOpen = _writer!.openReadWrite();
+    if (!didOpen) {
+      await disconnect();
+      throw SerialCannotOpen(device);
+    }
 
-	/// Disposes of the current [_reader] and [_writer] and marks them as null.
-	/// 
-	/// Use this to indicate that the previous device is no longer available.
-	Future<void> disconnect() async {
-		await dispose();
-		_writer = null;
-		_reader = null;
-	}
+    // Send a handshake, expect one back
+    await sendMessage(
+        Connect(sender: Device.DASHBOARD, receiver: Device.FIRMWARE));
+    await Future.delayed(const Duration(seconds: 1));
+    final received = _writer!.read(4); // nice way to read X bytes at a time
+    try {
+      final message = Connect.fromBuffer(received);
+      final isValid = message.receiver == Device.DASHBOARD;
+      if (!isValid) {
+        await disconnect();
+        throw SerialHandshakeFailed();
+      }
+      connectedDevice = message.sender;
+    } on InvalidProtocolBufferException {
+      await disconnect();
+      throw MalformedSerialPacket(packet: received);
+    }
+  }
 
-	/// Sends a [Message] over Serial, without wrapping it.
-	Future<void> sendMessage(Message message) => 
-		sendRaw(message.writeToBuffer());
+  /// Disposes of the current [_reader] and [_writer] and marks them as null.
+  ///
+  /// Use this to indicate that the previous device is no longer available.
+  Future<void> disconnect() async {
+    await dispose();
+    _writer = null;
+    _reader = null;
+    connectedDevice = null;
+  }
 
-	/// Write data to the device.
-	/// 
-	/// - If no device is connected, this will throw a [DeviceNotConnected].
-	/// - If there is an IO error, this will throw a [SerialPortError].
-	Future<void> sendRaw(Uint8List data) async {
-		if (_writer == null) throw DeviceNotConnected();
-		try {
-			_writer!.write(Uint8List.fromList(data), timeout: 500);
-		} on SerialPortError catch (error) {
-			await disconnect();
-			throw SerialIOError(error);
-		}
-	}
+  /// Sends a [Message] over Serial, without wrapping it.
+  Future<void> sendMessage(Message message) => sendRaw(message.writeToBuffer());
 
-	/// Returns a stream of all commands being sent.
-	/// 
-	/// - If no device is connected, this will throw a [DeviceNotConnected].
-	/// - If there is an IO error, this will throw a [SerialPortError].
-	Stream<Uint8List> get incomingData {
-		if (_reader == null) throw DeviceNotConnected();
-		try { return _reader!.stream; }
-		on SerialPortError catch (error) {
-			disconnect();
-			throw SerialIOError(error);
-		}
-	}
+  /// Write data to the device.
+  ///
+  /// - If no device is connected, this will throw a [DeviceNotConnected].
+  /// - If there is an IO error, this will throw a [SerialPortError].
+  Future<void> sendRaw(Uint8List data) async {
+    if (_writer == null) throw DeviceNotConnected();
+    try {
+      _writer!.write(Uint8List.fromList(data), timeout: 500);
+    } on SerialPortError catch (error) {
+      await disconnect();
+      throw SerialIOError(error);
+    }
+  }
+
+  /// Returns a stream of all commands being sent.
+  ///
+  /// - If no device is connected, this will throw a [DeviceNotConnected].
+  /// - If there is an IO error, this will throw a [SerialPortError].
+  Stream<Uint8List> get incomingData {
+    if (_reader == null) throw DeviceNotConnected();
+    try {
+      return _reader!.stream;
+    } on SerialPortError catch (error) {
+      disconnect();
+      throw SerialIOError(error);
+    }
+  }
 }
