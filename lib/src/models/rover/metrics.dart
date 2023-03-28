c883a87dd2ff5c08162615296006c4729feac81e
Aidan <aidanahram1@gmail.com>
2023-03-28 14:27:55 -0400

    tab fixing

--- lib/src/models/rover/metrics.dart
+++ lib/src/models/rover/metrics.dart
@@ -5,35 +5,35 @@ import "../model.dart";
 
 /// A data model that listens for updated data and provides [Metrics] to the UI.
 class RoverMetrics extends Model {
-  /// Data about the rover's core vitals.
-  final electrical = ElectricalMetrics();
+	/// Data about the rover's core vitals.
+	final electrical = ElectricalMetrics();
 
-  /// Data from the science subsystem.
-  final science = ScienceMetrics();
+	/// Data from the science subsystem.
+	final science = ScienceMetrics();
 
-  /// A list of all the metrics to iterate over.
-  ///
-  /// NOTE: Keep this as a getter, NOT a field. If this is made a field, then it won't update
-  /// when new data is received. As a getter, every time it is called it will use new data.
-  List<Metrics> get allMetrics => [electrical, science];
+	/// A list of all the metrics to iterate over.
+	///
+	/// NOTE: Keep this as a getter, NOT a field. If this is made a field, then it won't update
+	/// when new data is received. As a getter, every time it is called it will use new data.
+	List<Metrics> get allMetrics => [electrical, science];
 
-  /// Returns a function that updates a [Metrics] object and reloads the UI.
-  void Function(T) update<T extends Message>(Metrics<T> metrics) => (T data) {
-        metrics.update(data);
-        notifyListeners();
-      };
+	/// Returns a function that updates a [Metrics] object and reloads the UI.
+	void Function(T) update<T extends Message>(Metrics<T> metrics) => (T data) {
+		metrics.update(data);
+		notifyListeners();
+	};
 
-  @override
-  Future<void> init() async {
-    services.dataSocket.registerHandler<ElectricalData>(
-      name: ElectricalData().messageName,
-      decoder: ElectricalData.fromBuffer,
-      handler: update(electrical),
-    );
-    services.dataSocket.registerHandler<ScienceData>(
-      name: ScienceData().messageName,
-      decoder: ScienceData.fromBuffer,
-      handler: update(science),
-    );
-  }
+	@override
+	Future<void> init() async {
+		services.dataSocket.registerHandler<ElectricalData>(
+		name: ElectricalData().messageName,
+		decoder: ElectricalData.fromBuffer,
+		handler: update(electrical),
+		);
+		services.dataSocket.registerHandler<ScienceData>(
+			name: ScienceData().messageName,
+			decoder: ScienceData.fromBuffer,
+			handler: update(science),
+		);
+	}
 }
--- lib/src/services/serial.dart
+++ lib/src/services/serial.dart
@@ -16,107 +16,107 @@ export "serial_errors.dart";
 /// running, this service needs to explicitly connect to a device using [connect]. The device may
 /// be disconnected by calling [disconnect]. In any event, [dispose] will also call [disconnect].
 class Serial extends Service {
-  /// Writes data to the serial device.
-  ///
-  /// This field is only non-null after calling [connect] successfully.
-  SerialPort? _writer;
+	/// Writes data to the serial device.
+	///
+	/// This field is only non-null after calling [connect] successfully.
+	SerialPort? _writer;
 
-  /// Reads data from the serial device.
-  ///
-  /// This field is only non-null after calling [connect] successfully.
-  SerialPortReader? _reader;
+	/// Reads data from the serial device.
+	///
+	/// This field is only non-null after calling [connect] successfully.
+	SerialPortReader? _reader;
 
-  /// Device that is currently connected
-  ///
-  /// This field is only non-null after calling [connect] successfully.
-  /// This field gets assigned after revceiving response from the handshake
-  Device? connectedDevice;
+	/// Device that is currently connected
+	///
+	/// This field is only non-null after calling [connect] successfully.
+	/// This field gets assigned after revceiving response from the handshake
+	Device? connectedDevice;
 
-  @override
-  Future<void> init() async {}
+	@override
+	Future<void> init() async {}
 
-  @override
-  Future<void> dispose() async {
-    _writer?.dispose();
-    _reader?.close();
-  }
+	@override
+	Future<void> dispose() async {
+		_writer?.dispose();
+		_reader?.close();
+	}
 
-  /// All the Serial ports available to connect to.
-  ///
-  /// Not all of these will be for the firmware, so the user needs to choose the right one. To make
-  /// the display cleaner, this collection filters out duplicate entries.
-  Iterable<String> get availablePorts => SerialPort.availablePorts.toSet();
+	/// All the Serial ports available to connect to.
+	///
+	/// Not all of these will be for the firmware, so the user needs to choose the right one. To make
+	/// the display cleaner, this collection filters out duplicate entries.
+	Iterable<String> get availablePorts => SerialPort.availablePorts.toSet();
 
-  /// Connects a device to the dashboard for reading and writing.
-  ///
-  /// Sends a [Connect] message, then expects a [Connect] message in response.
-  Future<void> connect(String device) async {
-    await disconnect(); // only when exactly one new device is found
-    _writer = SerialPort(device);
-    _reader = SerialPortReader(_writer!);
-    final didOpen = _writer!.openReadWrite();
-    if (!didOpen) {
-      await disconnect();
-      throw SerialCannotOpen(device);
-    }
+	/// Connects a device to the dashboard for reading and writing.
+	///
+	/// Sends a [Connect] message, then expects a [Connect] message in response.
+	Future<void> connect(String device) async {
+		await disconnect(); // only when exactly one new device is found
+		_writer = SerialPort(device);
+		_reader = SerialPortReader(_writer!);
+		final didOpen = _writer!.openReadWrite();
+		if (!didOpen) {
+			await disconnect();
+			throw SerialCannotOpen(device);
+		}
 
-    // Send a handshake, expect one back
-    await sendMessage(
-        Connect(sender: Device.DASHBOARD, receiver: Device.FIRMWARE));
-    await Future.delayed(const Duration(seconds: 1));
-    final received = _writer!.read(4); // nice way to read X bytes at a time
-    try {
-      final message = Connect.fromBuffer(received);
-      final isValid = message.receiver == Device.DASHBOARD;
-      if (!isValid) {
-        await disconnect();
-        throw SerialHandshakeFailed();
-      }
-      connectedDevice = message.sender;
-    } on InvalidProtocolBufferException {
-      await disconnect();
-      throw MalformedSerialPacket(packet: received);
-    }
-  }
+		// Send a handshake, expect one back
+		await sendMessage(
+			Connect(sender: Device.DASHBOARD, receiver: Device.FIRMWARE));
+			await Future.delayed(const Duration(seconds: 1));
+			final received = _writer!.read(4); // nice way to read X bytes at a time
+		try {
+			final message = Connect.fromBuffer(received);
+			final isValid = message.receiver == Device.DASHBOARD;
+			if (!isValid) {
+				await disconnect();
+				throw SerialHandshakeFailed();
+			}
+			connectedDevice = message.sender;
+		} on InvalidProtocolBufferException {
+			await disconnect();
+			throw MalformedSerialPacket(packet: received);
+		}
+	}
 
-  /// Disposes of the current [_reader] and [_writer] and marks them as null.
-  ///
-  /// Use this to indicate that the previous device is no longer available.
-  Future<void> disconnect() async {
-    await dispose();
-    _writer = null;
-    _reader = null;
-    connectedDevice = null;
-  }
+	/// Disposes of the current [_reader] and [_writer] and marks them as null.
+	///
+	/// Use this to indicate that the previous device is no longer available.
+	Future<void> disconnect() async {
+		await dispose();
+		_writer = null;
+		_reader = null;
+		connectedDevice = null;
+	}
 
-  /// Sends a [Message] over Serial, without wrapping it.
-  Future<void> sendMessage(Message message) => sendRaw(message.writeToBuffer());
+	/// Sends a [Message] over Serial, without wrapping it.
+	Future<void> sendMessage(Message message) => sendRaw(message.writeToBuffer());
 
-  /// Write data to the device.
-  ///
-  /// - If no device is connected, this will throw a [DeviceNotConnected].
-  /// - If there is an IO error, this will throw a [SerialPortError].
-  Future<void> sendRaw(Uint8List data) async {
-    if (_writer == null) throw DeviceNotConnected();
-    try {
-      _writer!.write(Uint8List.fromList(data), timeout: 500);
-    } on SerialPortError catch (error) {
-      await disconnect();
-      throw SerialIOError(error);
-    }
-  }
+	/// Write data to the device.
+	///
+	/// - If no device is connected, this will throw a [DeviceNotConnected].
+	/// - If there is an IO error, this will throw a [SerialPortError].
+	Future<void> sendRaw(Uint8List data) async {
+		if (_writer == null) throw DeviceNotConnected();
+		try {
+			_writer!.write(Uint8List.fromList(data), timeout: 500);
+		} on SerialPortError catch (error) {
+			await disconnect();
+			throw SerialIOError(error);
+		}
+	}
 
-  /// Returns a stream of all commands being sent.
-  ///
-  /// - If no device is connected, this will throw a [DeviceNotConnected].
-  /// - If there is an IO error, this will throw a [SerialPortError].
-  Stream<Uint8List> get incomingData {
-    if (_reader == null) throw DeviceNotConnected();
-    try {
-      return _reader!.stream;
-    } on SerialPortError catch (error) {
-      disconnect();
-      throw SerialIOError(error);
-    }
-  }
+	/// Returns a stream of all commands being sent.
+	///
+	/// - If no device is connected, this will throw a [DeviceNotConnected].
+	/// - If there is an IO error, this will throw a [SerialPortError].
+	Stream<Uint8List> get incomingData {
+		if (_reader == null) throw DeviceNotConnected();
+		try {
+			return _reader!.stream;
+		} on SerialPortError catch (error) {
+			disconnect();
+			throw SerialIOError(error);
+		}
+	}
 }
