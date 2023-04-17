6393343b2be01d118916f2b500a7e75c0b837819
Levi Lesches <levilesches@gmail.com>
2023-04-17 00:51:08 -0400

    Fixed "intuitive" drive commands"

--- lib/src/models/rover/controls/drive.dart
+++ lib/src/models/rover/controls/drive.dart
@@ -17,25 +17,24 @@ class DriveControls extends RoverControls {
 	@override
 	List<Message> parseInputs(GamepadState state) => [
 		// Manual controls
-		DriveCommand(setLeft: true, left: -1*state.leftThumbstickY.normalizeJoystick.clamp(-1, 1)),
-		DriveCommand(setRight: true, right: state.rightThumbstickY.normalizeJoystick.clamp(-1, 1)),
-		if (state.dpadUp) updateThrottle(throttleIncrement),
-		if (state.dpadDown) updateThrottle(-throttleIncrement),
-
+		if (state.dpadUp) updateThrottle(throttleIncrement)
+		else if (state.dpadDown) updateThrottle(-throttleIncrement)
 		// More intuitive controls
-		if (state.leftShoulder) ...[
-			DriveCommand(setLeft: true, left: 1),
+		else if (state.leftShoulder) ...[
+			DriveCommand(setLeft: true, left: -1),
 			DriveCommand(setRight: true, right: 1),
 		] else if (state.rightShoulder)...[
-			DriveCommand(setLeft: true, left: -1),
+			DriveCommand(setLeft: true, left: 1),
 			DriveCommand(setRight: true, right: -1),
-		],
-		if (state.leftTrigger > 0) ...[
-			DriveCommand(setLeft: true, left: state.normalLeftTrigger),
-			DriveCommand(setRight: true, right: -1*state.normalRightTrigger),
-		] else if (state.rightTrigger > 0)...[
+		] else if (state.leftTrigger > 0) ...[
 			DriveCommand(setLeft: true, left: -1*state.normalLeftTrigger),
+			DriveCommand(setRight: true, right: -1*state.normalLeftTrigger),
+		] else if (state.rightTrigger > 0)...[
+			DriveCommand(setLeft: true, left: state.normalRightTrigger),
 			DriveCommand(setRight: true, right: state.normalRightTrigger),
+		] else ...[
+			DriveCommand(setLeft: true, left: -1*state.leftThumbstickY.normalizeJoystick.clamp(-1, 1)),
+			DriveCommand(setRight: true, right: state.rightThumbstickY.normalizeJoystick.clamp(-1, 1)),
 		]
 	];
 
