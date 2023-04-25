c7443d60e976771e51a33e49b54872948aefb175
Levi Lesches <levilesches@gmail.com>
2023-04-25 06:23:56 -0400

    Documented

--- lib/src/pages/splash.dart
+++ lib/src/pages/splash.dart
@@ -4,13 +4,19 @@ import "package:rover_dashboard/models.dart";
 import "package:rover_dashboard/pages.dart";
 import "package:rover_dashboard/services.dart";
 
+/// Initializes the dashboard and handles errors.
 class SplashPage extends StatefulWidget {
 	@override
 	SplashPageState createState() => SplashPageState();
 }
 
+/// Initializes the dashboard and handles errors.
 class SplashPageState extends State<SplashPage>{
-	String? errorText, current;
+	/// The error message produced during initialization, if any.
+	String? errorText;
+
+	/// The current task, if any.
+	String? current;
 
 	@override
 	void initState() {
@@ -18,6 +24,7 @@ class SplashPageState extends State<SplashPage>{
 		init();
 	}
 
+	/// Calls [Services.init] and [Models.init] while monitoring for errors. 
 	Future<void> init() async {
 		try {
 			current = "Flutter";
