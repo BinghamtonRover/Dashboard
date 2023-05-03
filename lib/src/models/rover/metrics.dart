08db72e906ddeacd5d54962fcdb6cc59015d449f
Levi Lesches <levilesches@gmail.com>
2023-05-03 06:15:55 -0400

    Drive data

--- lib/src/models/rover/metrics.dart
+++ lib/src/models/rover/metrics.dart
@@ -15,11 +15,14 @@ class RoverMetrics extends Model {
   /// Data from the GPS.
   final position = PositionMetrics();
 
+  // Data from the drive subsystem.
+  final drive = DriveMetrics();
+
 	/// A list of all the metrics to iterate over.
 	///
 	/// NOTE: Keep this as a getter, NOT a field. If this is made a field, then it won't update
 	/// when new data is received. As a getter, every time it is called it will use new data.
-	List<Metrics> get allMetrics => [electrical, science, position];
+	List<Metrics> get allMetrics => [electrical, drive, position, science];
 
 	/// Returns a function that updates a [Metrics] object and reloads the UI.
 	void Function(T) update<T extends Message>(Metrics<T> metrics) => (data) {
@@ -34,6 +37,16 @@ class RoverMetrics extends Model {
 			name: ElectricalData().messageName,
 			decoder: ElectricalData.fromBuffer,
 			handler: update(electrical),
+		);
+		services.dataSocket.registerHandler<DriveData>(
+			name: DriveData().messageName,
+			decoder: DriveData.fromBuffer,
+			handler: (data) {
+				if (data.setLeft) drive.data.left = data.left;
+				if (data.setRight) drive.data.right = data.right;
+				if (data.setThrottle) drive.data.throttle = data.throttle;
+				notifyListeners();
+			},
 		);
 		services.dataSocket.registerHandler<ScienceData>(
 			name: ScienceData().messageName,
