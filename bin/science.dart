f6ef282fca859e42d0584ac127bbd413fc805822
Levi Lesches <levilesches@gmail.com>
2023-05-05 02:15:28 -0400

    Revamped bin file

--- bin/science.dart
+++ bin/science.dart
@@ -1,20 +1,14 @@
 import "dart:io";
 import "dart:math";
+import "dart:convert";
 
 import "package:rover_dashboard/data.dart";
 
 /// Change these paths as needed
-const oldPath = r"C:\Users\levi\Documents\Dashboard\logs\2023-4-28-18-53\ScienceData.log";
-const newPath = r"C:\Users\levi\Documents\Dashboard\logs\2023-4-28-18-53\ScienceData2.log";
+const oldPath = r"C:\Users\llesche1\Documents\Dashboard\logs\2023-5-5-1-7\ScienceData.log";
+const newPath = r"C:\Users\llesche1\Documents\Dashboard\logs\2023-5-5-1-7\ScienceData2.log";
 
-final random = Random();
 
-extension on Timestamp {
-	/// Use this to add a [Duration] to a [Timestamp] object.
-	/// 
-	/// Example: timestamp.add(Duration(seconds: 2))
-	Timestamp addDuration(Duration duration) => Timestamp.fromDateTime(toDateTime().add(duration));
-}
 
 WrappedMessage? updateData(WrappedMessage wrapper) {
 	// Return a new message based on this one.
@@ -29,27 +23,33 @@ WrappedMessage? updateData(WrappedMessage wrapper) {
 	// For example, to delete messages with a pH over 14: 
 	// if (data.pH > 14) return null;
 	// else return data;
+	if (data.co2 >= 2000) return null;
+	if (data.pH > 14) return null;
+	if (data.methane > 500) return null;
+	if (data.humidity > 50) return null;
 
-	return WrappedMessage(
-		timestamp: timestamp.addDuration(const Duration(seconds: 1)), 
-		data: data.writeToBuffer(),
-	);
+	else return wrapper;
+	// return WrappedMessage(
+	// 	timestamp: timestamp, 
+	// 	data: data.writeToBuffer(),
+	// 	name: "ScienceData",
+	// );
 }
 
 List<WrappedMessage> addNewData() => [
-	for (int s = 0; s < 3; s++) 
-		for (int i = 0; i < 20; i++) WrappedMessage(
-			name: "ScienceData",
-			timestamp: Timestamp.fromDateTime(DateTime.now().add(Duration(seconds: i + 1))), 
-			data: ScienceData(
-				sample: s, 
-				temperature: i + s + (random.nextInt(10).toDouble()),
-				methane: i + s + (random.nextInt(7).toDouble()),
-				co2: i + s + (random.nextInt(5).toDouble()),
-				humidity: i + s + (random.nextInt(13).toDouble()),
-				pH: i + s + (random.nextInt(15).toDouble()),
-			).writeToBuffer(),
-		),
+	// for (int s = 0; s < 3; s++) 
+	// 	for (int i = 0; i < 20; i++) WrappedMessage(
+	// 		name: "ScienceData",
+	// 		timestamp: Timestamp.fromDateTime(DateTime.now().add(Duration(seconds: i + 1))), 
+	// 		data: ScienceData(
+	// 			// sample: s, 
+	// 			temperature: i + s + (random.nextInt(10).toDouble()),
+	// 			methane: i + s + (random.nextInt(7).toDouble()),
+	// 			co2: i + s + (random.nextInt(5).toDouble()),
+	// 			humidity: i + s + (random.nextInt(13).toDouble()),
+	// 			pH: i + s + (random.nextInt(15).toDouble()),
+	// 		).writeToBuffer(),
+	// 	),
 ];
 
 
@@ -63,33 +63,43 @@ List<WrappedMessage> addNewData() => [
 
 // ======= Do not edit =======
 
+final random = Random();
+
+extension on Timestamp {
+	/// Use this to add a [Duration] to a [Timestamp] object.
+	/// 
+	/// Example: timestamp.add(Duration(seconds: 2))
+	Timestamp addDuration(Duration duration) => Timestamp.fromDateTime(toDateTime().add(duration));
+}
+
 /// Outputs log data to the correct file based on message
-Future<void> logData(WrappedMessage message) async {
+Future<void> logData(Message message) async{
   final List<int> bytes = message.writeToBuffer();
-  final String line = bytes.join(", ");
+  final line = base64.encode(bytes);
   final file = File(newPath);
-  await file.writeAsString("$line\n", mode: FileMode.writeOnlyAppend, flush: true);
+  await file.writeAsString("$line\n", mode: FileMode.append, flush: true);
 }
 
 /// Reads logs from the given file.
 Future<List<WrappedMessage>> readLogs(File file) async => [
   for (final line in (await file.readAsString()).trim().split("\n"))
-    WrappedMessage.fromBuffer([
-      for (final byte in line.split(", ")) int.parse(byte)
-    ])
+    WrappedMessage.fromBuffer(base64.decode(line))
 ];
 
 void main() async {
-	await File(newPath).delete();
+	if (File(newPath).existsSync()) await File(newPath).delete();
 	final List<WrappedMessage> oldData = await readLogs(File(oldPath));
 	final List<WrappedMessage?> newData = [
 		for (final data in oldData) updateData(data),
-		...addNewData(),
+		// ...addNewData(),
 	];
+	int counter = 0;
 	for (final data in newData) {
+		if (data == null) counter++;
 		if (data == null) continue;
 		await logData(data);
 	}
+	print("Pruned $counter logs");
 }
 
 // ======= Do not edit =======
