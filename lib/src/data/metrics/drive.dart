1cbc79ca7ed120712af68d6e4461ef7b32b5b116
Levi Lesches <levilesches@gmail.com>
2023-05-08 05:37:03 -0400

    Restored `comment_references` lint

--- lib/src/data/metrics/drive.dart
+++ lib/src/data/metrics/drive.dart
@@ -3,7 +3,7 @@ import "package:rover_dashboard/data.dart";
 /// Metrics reported by the drive subsystem.
 /// 
 /// In the future, the drive Teensy will have a GPS and IMU to read [RoverPosition] data. For now,
-/// this information is sent separately and is represented by its own [RoverMetrics] object because
+/// this information is sent separately and is represented by its own [RoverPosition] object because
 /// they are collected on the Pi. In the future, when they are moved to the Drive subsystem, this
 /// data should still be kept separate so as to make it easier to show in the UI and send to MARS.
 class DriveMetrics extends Metrics<DriveData> {
