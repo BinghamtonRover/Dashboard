1bdb30f00e6ac95a096b91e20911b5c7de1d29bf
Levi Lesches <levilesches@gmail.com>
2023-05-09 12:48:38 -0400

    Added function to change LEDs strip

--- lib/src/models/rover/settings.dart
+++ lib/src/models/rover/settings.dart
@@ -12,8 +12,8 @@ const confirmationDelay = Duration(milliseconds: 100);
 /// message in response (see [UpdateSetting]). If we do not get the response after waiting for a
 /// confirmationDelay], we conclude that the rover didn't receive our request, similar to heartbeat.
 class RoverSettings extends Model {
-	/// The status of the rover.
-	RoverStatus status = RoverStatus.MANUAL;
+	/// The settings we will send to the rover.
+	UpdateSetting settings = UpdateSetting(status: RoverStatus.MANUAL);
 
 	/// The last received confirmation
 	UpdateSetting? _confirmation;
@@ -27,19 +27,41 @@ class RoverSettings extends Model {
 		);
 	}
 
+	/// Sends an [UpdateSetting] and awaits a response.
+	/// 
+	/// The response must be an echo of the data sent, to ensure the rover acknowledges the data.
+	Future<bool> tryChangeSettings(UpdateSetting value) async {
+		services.dataSocket.sendMessage(value);
+		await Future<void>.delayed(confirmationDelay);
+		return value == _confirmation;
+	}
+
 	/// Sets the status of the rover.
 	/// 
 	/// See [RoverStatus] for details.
 	Future<void> setStatus(RoverStatus value) async {
 		final message = UpdateSetting(status: value);
-		services.dataSocket.sendMessage(message);
-		await Future<void>.delayed(confirmationDelay);
-		if (message == _confirmation) {
+		if (await tryChangeSettings(message)) {
 			models.home.setMessage(severity: Severity.info, text: "Set mode to ${value.humanName}");
-			status = value;
+			settings.status = value;
 			notifyListeners();
 		} else { 
 			models.home.setMessage(severity: Severity.error, text: "Failed to set status"); 
 		}
 	} 
+
+	/// Changes the color of the rover's LED strip.
+	Future<bool> setColor(double red, double green, double blue) async {
+		final value = ProtoColor(red: red, green: green, blue: blue);
+		final message = UpdateSetting(color: value);
+		if (await tryChangeSettings(message)) {
+			models.home.setMessage(severity: Severity.info, text: "Changed color to ($red, $green, $blue)");
+			settings.color = value;
+			notifyListeners();
+			return true;
+		} else { 
+			models.home.setMessage(severity: Severity.error, text: "Failed to set color"); 
+			return false;
+		}
+	}
 }
