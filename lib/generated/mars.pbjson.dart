6dfec1e1e4fffb0449ed217cf11d6ac45646ca4b
Levi Lesches <levilesches@gmail.com>
2023-05-23 20:00:26 -0400

    Updated MARS data and commands

--- lib/generated/mars.pbjson.dart
+++ lib/generated/mars.pbjson.dart
@@ -14,20 +14,22 @@ const MarsCommand$json = const {
   '2': const [
     const {'1': 'swivel', '3': 1, '4': 1, '5': 2, '10': 'swivel'},
     const {'1': 'tilt', '3': 2, '4': 1, '5': 2, '10': 'tilt'},
-    const {'1': 'position', '3': 3, '4': 1, '5': 11, '6': '.RoverPosition', '10': 'position'},
+    const {'1': 'position', '3': 3, '4': 1, '5': 11, '6': '.GpsCoordinates', '10': 'position'},
+    const {'1': 'baseStationOverride', '3': 4, '4': 1, '5': 11, '6': '.GpsCoordinates', '10': 'baseStationOverride'},
   ],
 };
 
 /// Descriptor for `MarsCommand`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List marsCommandDescriptor = $convert.base64Decode('CgtNYXJzQ29tbWFuZBIWCgZzd2l2ZWwYASABKAJSBnN3aXZlbBISCgR0aWx0GAIgASgCUgR0aWx0EioKCHBvc2l0aW9uGAMgASgLMg4uUm92ZXJQb3NpdGlvblIIcG9zaXRpb24=');
+final $typed_data.Uint8List marsCommandDescriptor = $convert.base64Decode('CgtNYXJzQ29tbWFuZBIWCgZzd2l2ZWwYASABKAJSBnN3aXZlbBISCgR0aWx0GAIgASgCUgR0aWx0EisKCHBvc2l0aW9uGAMgASgLMg8uR3BzQ29vcmRpbmF0ZXNSCHBvc2l0aW9uEkEKE2Jhc2VTdGF0aW9uT3ZlcnJpZGUYBCABKAsyDy5HcHNDb29yZGluYXRlc1ITYmFzZVN0YXRpb25PdmVycmlkZQ==');
 @$core.Deprecated('Use marsDataDescriptor instead')
 const MarsData$json = const {
   '1': 'MarsData',
   '2': const [
     const {'1': 'swivel', '3': 1, '4': 1, '5': 2, '10': 'swivel'},
     const {'1': 'tilt', '3': 2, '4': 1, '5': 2, '10': 'tilt'},
+    const {'1': 'coordinates', '3': 3, '4': 1, '5': 11, '6': '.GpsCoordinates', '10': 'coordinates'},
   ],
 };
 
 /// Descriptor for `MarsData`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List marsDataDescriptor = $convert.base64Decode('CghNYXJzRGF0YRIWCgZzd2l2ZWwYASABKAJSBnN3aXZlbBISCgR0aWx0GAIgASgCUgR0aWx0');
+final $typed_data.Uint8List marsDataDescriptor = $convert.base64Decode('CghNYXJzRGF0YRIWCgZzd2l2ZWwYASABKAJSBnN3aXZlbBISCgR0aWx0GAIgASgCUgR0aWx0EjEKC2Nvb3JkaW5hdGVzGAMgASgLMg8uR3BzQ29vcmRpbmF0ZXNSC2Nvb3JkaW5hdGVz');
