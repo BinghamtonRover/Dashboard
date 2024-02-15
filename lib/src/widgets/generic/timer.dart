d284cc09c66f42a081958ab128fa4028aa8b91e0
Levi Lesches <levilesches@gmail.com>
2024-02-15 15:41:35 -0500

    Upgraded HomePage to ReactiveWidget

---
+++ lib/src/widgets/generic/timer.dart
@@ -0,0 +1,50 @@
+import "package:flutter/material.dart";
+
+import "package:rover_dashboard/models.dart";
+import "package:rover_dashboard/widgets.dart";
+
+/// A widget to view timer 
+/// Can also stop and start timer
+class TimerWidget extends ReusableReactiveWidget<MissionTimer> {
+  /// Creates a new Timer widget
+  TimerWidget() : super(models.home.mission);
+  
+  /// Gets the text style for the timer.
+  TextStyle getStyle(BuildContext context, MissionTimer model) => model.underMin
+    ? context.textTheme.headlineSmall!.copyWith(
+      color: context.colorScheme.error,
+      fontWeight: FontWeight.bold,
+    )
+    : context.textTheme.headlineSmall!.copyWith(color: context.colorScheme.onPrimary);
+  
+	@override
+	Widget build(BuildContext context, MissionTimer model) => model.title == null
+    ? Container()
+    : Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("${model.title}: ",
+          style: context.textTheme.headlineSmall!.copyWith(color: context.colorScheme.onPrimary),
+        ),
+        const SizedBox(width: 4),
+        AnimatedScale(
+          scale: (model.underMin) && (model.timeLeft.inSeconds.isEven) ? 1.2 : 1, 
+          duration: const Duration(milliseconds: 750),
+          child: Text(
+            model.timeLeft.toString().split(".").first.padLeft(8, "0"),
+            style: getStyle(context, model),
+          ),
+        ),
+        const SizedBox(width: 8),
+        ElevatedButton(
+          onPressed: model.isPaused ? model.resume : model.pause,
+          child: model.isPaused ? const Text("Resume") : const Text("Pause"), 
+        ),
+        const SizedBox(width: 8),
+        ElevatedButton(
+          onPressed: model.cancel,
+          child: const Text("Cancel"), 
+        ),
+      ],
+    );
+}
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -23,6 +23,7 @@ export "src/widgets/atomic/video_feed.dart";
 
 export "src/widgets/generic/gamepad.dart";
 export "src/widgets/generic/reactive_widget.dart";
+export "src/widgets/generic/timer.dart";
 
 export "src/widgets/navigation/footer.dart";
 export "src/widgets/navigation/sidebar.dart";
