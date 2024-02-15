7f502d165b7d5558671cfb337a2b55800fe55b98
Levi Lesches <levilesches@gmail.com>
2024-02-15 15:28:14 -0500

    Made views resizable

--- lib/src/widgets/navigation/views.dart
+++ lib/src/widgets/navigation/views.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
+import "package:flutter_resizable_container/flutter_resizable_container.dart";
 
+import "package:rover_dashboard/data.dart";
 import "package:rover_dashboard/models.dart";
 import "package:rover_dashboard/widgets.dart";
 
@@ -8,31 +10,103 @@ class ViewsWidget extends StatelessWidget {
 	/// A const constructor.
 	const ViewsWidget();
 
-	/// Renders the view at the given [index] in [ViewsModel.views].
-	Widget getView(BuildContext context, int index) => Expanded(child: Container(
-		decoration: BoxDecoration(border: Border.all(width: 3)),
-		child: models.views.views[index].builder(context),
-	),);
-
 	@override
 	Widget build(BuildContext context) => ProviderConsumer<ViewsModel>.value(
 		value: models.views,
-		builder: (model) => Column(children: [
-			Expanded(
-				child: Row(children: [
-					if (model.views.isNotEmpty) getView(context, 0),
-					if (model.views.length >= 3) getView(context, 1),
-				],
-			),),
-			if (model.views.length >= 2) Expanded(
-				child: Row(children: [
-					if (model.views.length >= 2) 
-						// Put the 2nd view on the bottom row or the upper left corner
-						if (model.views.length >= 3) getView(context, 2)
-						else getView(context, 1),
-					if (model.views.length >= 4) getView(context, 3),
-				],),
-			),
-		],),
+		builder: (model) => LayoutBuilder(
+      builder: (context, constraints) => switch (model.views.length) {
+      1 => Column(children: [Expanded(child: models.views.views.first.builder(context))]),
+      2 => ResizableContainer(
+        direction: switch (models.settings.dashboard.splitMode) {
+          SplitMode.horizontal => Axis.vertical,
+          SplitMode.vertical => Axis.horizontal,
+        },
+        dividerWidth: 5, 
+        dividerColor: Colors.black,
+        children: [
+          ResizableChildData(
+            startingRatio: 0.5,
+            child: models.views.views[0].builder(context),
+          ),
+          ResizableChildData(
+            startingRatio: 0.5,
+            child: models.views.views[1].builder(context),
+          ),
+        ],
+      ),
+      3 => ResizableContainer(
+        direction: Axis.vertical,
+        dividerWidth: 5, 
+        dividerColor: Colors.black,
+        children: [
+          ResizableChildData(
+            startingRatio: 0.5,
+            child: ResizableContainer(
+              direction: Axis.horizontal,
+              dividerWidth: 5, 
+              dividerColor: Colors.black,
+              children: [
+                ResizableChildData(
+                  startingRatio: 0.5,
+                  child: models.views.views[0].builder(context),
+                ),
+                ResizableChildData(
+                  startingRatio: 0.5,
+                  child: models.views.views[1].builder(context),
+                ),
+              ],
+            ),
+          ),
+          ResizableChildData(
+            startingRatio: 0.5,
+            child: models.views.views[2].builder(context),
+          ),
+        ],
+      ),
+      4 => ResizableContainer(
+        direction: Axis.vertical,
+        dividerWidth: 5, 
+        dividerColor: Colors.black,
+        children: [
+          ResizableChildData(
+            startingRatio: 0.5,
+            child: ResizableContainer(
+              direction: Axis.horizontal,
+              dividerWidth: 5, 
+              dividerColor: Colors.black,
+              children: [
+                ResizableChildData(
+                  startingRatio: 0.5,
+                  child: models.views.views[0].builder(context),
+                ),
+                ResizableChildData(
+                  startingRatio: 0.5,
+                  child: models.views.views[1].builder(context),
+                ),
+              ],
+            ),
+          ),
+          ResizableChildData(
+            startingRatio: 0.5,
+            child: ResizableContainer(
+              direction: Axis.horizontal,
+              dividerWidth: 5, 
+              dividerColor: Colors.black,
+              children: [
+                ResizableChildData(
+                  startingRatio: 0.5,
+                  child: models.views.views[2].builder(context),
+                ),
+                ResizableChildData(
+                  startingRatio: 0.5,
+                  child: models.views.views[3].builder(context),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      _ => throw StateError("Too many views: ${model.views.length}"),
+    },),
 	);
 }
