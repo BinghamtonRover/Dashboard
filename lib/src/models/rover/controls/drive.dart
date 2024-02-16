feeeeb75c4b00c5d9958fc0734ad318ecd3da560
Levi Lesches <levilesches@gmail.com>
2024-02-16 03:11:17 -0500

    Hopefully improved drive controls

--- lib/src/models/rover/controls/drive.dart
+++ lib/src/models/rover/controls/drive.dart
@@ -5,40 +5,14 @@ import "controls.dart";
 
 /// A [RoverControls] that drives the rover.
 class DriveControls extends RoverControls {
-	/// Increases the throttle by +/- 20%.
-	static const throttleIncrement = 0.1;
-
-	/// The current throttle, as a percentage of the rover's top speed.
-	double throttle = 0;
-
 	@override
 	OperatingMode get mode => OperatingMode.drive;
 
 	@override
 	List<Message> parseInputs(GamepadState state) => [
-		// // Adjust throttle
-		// if (state.dpadUp) updateThrottle(throttleIncrement)
-		// else if (state.dpadDown) updateThrottle(-throttleIncrement),
-		// Adjust wheels 
 		DriveCommand(setLeft: true, left: state.normalLeftY),
 		DriveCommand(setRight: true, right: -1*state.normalRightY),
-		// More intuitive controls
-		// if (state.normalShoulder != 0) ...[
-		// 	DriveCommand(setLeft: true, left: state.normalShoulder),
-		// 	DriveCommand(setRight: true, right: state.normalShoulder),
-		// ],
-		// if (state.normalTrigger != 0) ...[
-		// 	DriveCommand(setLeft: true, left: state.normalTrigger),
-		// 	DriveCommand(setRight: true, right: -1 * state.normalTrigger),
-		// ],
 	];
-
-	/// Updates the throttle by [throttleIncrement], clamping at [0, 1].
-	Message updateThrottle(double value) {
-		throttle += value;
-		throttle = throttle.clamp(0, 1);
-		return DriveCommand(setThrottle: true, throttle: throttle);
-	}
 
 	@override
 	List<Message> get onDispose => [
@@ -51,8 +25,7 @@ class DriveControls extends RoverControls {
 	Map<String, String> get buttonMapping => {
 		"Left Throttle": "Left joystick (vertical)",
 		"Right Throttle": "Right joystick (vertical)",
-		"Drive Straight": "Triggers",
-		"Turn in place": "Bumpers",
-		"Adjust speed": "D-pad up/down",
+		// "Drive Straight": "Triggers",
+		// "Turn in place": "Bumpers",
 	};
 }
