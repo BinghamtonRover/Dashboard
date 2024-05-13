14e0d2f00319d84bbbb5f9d5e5e8fcc6b6746d40
Levi Lesches <levilesches@gmail.com>
2024-05-12 20:25:09 -0400

    Fixed controls

--- lib/src/models/rover/controls/science.dart
+++ lib/src/models/rover/controls/science.dart
@@ -9,7 +9,7 @@ import "controls.dart";
 /// A [RoverControls] that controls the science chamber.
 class ScienceControls extends RoverControls {
   /// Whether the shoulder buttons should move tubes or steps.
-  bool tubeMode = true;
+  bool tubeMode = false;
   /// The amount of steps to move the dirt carousel when the button is held.
   double carouselIncrement = 1000;
   /// The amount of steps to move the scooper when the button is held.
@@ -24,8 +24,8 @@ class ScienceControls extends RoverControls {
   /// -1 if the left bumper was pressed, +1 if the right bumper was pressed, 0 otherwise.
   int bumperFlag = 0;
   
-	@override
-	OperatingMode get mode => OperatingMode.science;
+  @override
+  OperatingMode get mode => OperatingMode.science;
 
   @override
   void updateState(GamepadState state) {
@@ -36,17 +36,17 @@ class ScienceControls extends RoverControls {
     rightBumper = state.rightShoulder;
   }
 
-	@override
-	List<Message> parseInputs(GamepadState state) => [
+  @override
+  List<Message> parseInputs(GamepadState state) => [
     if (tubeMode) ...[
       if (bumperFlag == -1) ScienceCommand(carousel: CarouselCommand.PREV_TUBE),
       if (bumperFlag == 1) ScienceCommand(carousel: CarouselCommand.NEXT_TUBE),
     ] else ...[
-      if (bumperFlag != 0) ScienceCommand(carouselMotor: carouselIncrement * bumperFlag),
+      if (state.normalShoulder != 0) ScienceCommand(carouselMotor: carouselIncrement * state.normalShoulder),
     ],
         
-		if (state.normalLeftY != 0) ScienceCommand(subsurfaceMotor: subsurfaceIncrement * state.normalLeftY),
-		if (state.normalRightY != 0) ScienceCommand(scoopMotor: scoopIncrement * state.normalRightY),
+    if (state.normalLeftY != 0) ScienceCommand(subsurfaceMotor: subsurfaceIncrement * state.normalLeftY),
+    if (state.normalRightY != 0) ScienceCommand(scoopMotor: scoopIncrement * state.normalRightY),
 
     if (state.buttonA) ScienceCommand(pumps: PumpState.PUMP_ON)
     else ScienceCommand(pumps: PumpState.PUMP_OFF),
@@ -57,15 +57,15 @@ class ScienceControls extends RoverControls {
     if (state.dpadUp) ScienceCommand(scoop: ServoState.SERVO_OPEN),
     if (state.dpadDown) ScienceCommand(scoop: ServoState.SERVO_CLOSE),
 
-		if (state.buttonStart) ScienceCommand(calibrate: true),
-		if (state.buttonBack) ScienceCommand(stop: true),
-	];
+    if (state.buttonStart) ScienceCommand(calibrate: true),
+    if (state.buttonBack) ScienceCommand(stop: true),
+  ];
 
-	@override
-	List<Message> get onDispose => [ScienceCommand(stop: true)];
+  @override
+  List<Message> get onDispose => [ScienceCommand(stop: true)];
 
-	@override
-	Map<String, String> get buttonMapping => {
+  @override
+  Map<String, String> get buttonMapping => {
     if (tubeMode) 
       "Dirt carousel": "Left and right shoulders"
     else 
@@ -75,5 +75,5 @@ class ScienceControls extends RoverControls {
     "Open/Close scoop": "D-pad Up/Down",
     "Activate pumps": "A (hold)",
     "Open funnel": "B (hold)",
-	};
+  };
 }
