6d47404e3e79a81d99e367023171a3dee1a04d3b
max-mazer <maxmazer04@gmail.com>
2024-09-15 21:08:49 -0400

    Added Expanded widet to Containers to Improve UI, Fixed Image Paths in both pubspec and rocks.dart

--- lib/src/pages/rocks.dart
+++ lib/src/pages/rocks.dart
@@ -7,72 +7,72 @@ class RockModel with ChangeNotifier {
   final rocks = <Rock>[
     const Rock(
       "Shale",
-      "assets/Rocks & Minerals/Shale.jpg",
+      "assets/Rocks_Minerals_Images/Shale.jpg",
       "Has to be a carbonate-rich enviroment to cause dark coloring",
     ),
     const Rock(
       "Mudstone",
-      "assets/Rocks & Minerals/mudstone1.jpg",
+      "assets/Rocks_Minerals_Images/mudstone1.jpg",
       "Clastic fine-grained, generally soft, variable color, clay silt",
     ),
      const Rock(
       "Limestone",
-      "assets/Rocks & Minerals/Limestone.jpg",
+      "assets/Rocks_Minerals_Images/Limestone.jpg",
       "Off white-gray, shallow marine enviroments, could contain fossils, chalky \n   \u2022 oolite, Chalk, Dense Limestone, Crystalline Limestone, Coquina, Micrite, Tavertine",
     ),
     const Rock(
       "Dolomites/Dolostones",
-      "assets/Rocks & Minerals/Dolomite1.jpg",
+      "assets/Rocks_Minerals_Images/Dolomite1.jpg",
       "Carbonate sedimentary rock",
     ),
     const Rock(
       "Conglomerates",
-      "assets/Rocks & Minerals/Conglomerate.jpg",
+      "assets/Rocks_Minerals_Images/Conglomerate.jpg",
       "Visible, rounded clasts within the matrix",
     ),
    const Rock(
       "Basalt",
-      "assets/Rocks & Minerals/Basalt.jpg",
+      "assets/Rocks_Minerals_Images/Basalt.jpg",
       "Fine-grained, dark coloring, no visible crystals",
     ),
      const Rock(
       "Sandstone",
-      "assets/Rocks & Minerals/Sandstone.jpg",
+      "assets/Rocks_Minerals_Images/Sandstone.jpg",
       "Grainy appearance",
     ),
     const Rock(
       "Phyllosilicates",
-      "assets/Rocks & Minerals/Phyllosilicates.jpg",
+      "assets/Rocks_Minerals_Images/Phyllosilicates.jpg",
       "Clay \n   \u2022 Layers of tetrahedral and octahedral sheets",
     ),
     const Rock(
       "Carbonates",
-      "assets/Rocks & Minerals/Carbonate.jpg",
+      "assets/Rocks_Minerals_Images/Carbonate.jpg",
       "Precipitate from water \n   \u2022 Fizzies when hydrochloric acid is placed on it",
     ),
     const Rock(
       "Hematite",
-      "assets/Rocks & Minerals/Hematite.jpg",
+      "assets/Rocks_Minerals_Images/Hematite.jpg",
       "Precipitate from water \n   \u2022 Deep red or brownish red streak",
     ),   
     const Rock(
       "Olivine",
-      "assets/Rocks & Minerals/Olivine.jpg",
+      "assets/Rocks_Minerals_Images/Olivine.jpg",
       "Weathers in the presence of water \n   \u2022 Green or pale green, lack of cleavage, in rocks it's usually rounded",
     ), 
      const Rock(
       "Pyroxene",
-      "assets/Rocks & Minerals/Pyroxene.jpg",
+      "assets/Rocks_Minerals_Images/Pyroxene.jpg",
       "Weathers in the presence of water \n   \u2022 Has cleavage, dark green to black",
     ), 
        const Rock(
       "Pigeonite",
-      "assets/Rocks & Minerals/Pigeonite.jpg",
+      "assets/Rocks_Minerals_Images/Pigeonite.jpg",
       "Found on meteors",
     ),
     const Rock(
       "Augite",
-      "assets/Rocks & Minerals/Pyroxene.jpg",
+      "assets/Rocks_Minerals_Images/Pyroxene.jpg",
       "Found on meteors \n   \u2022 Greenish white streak",
     ),       
   ];
@@ -135,32 +135,39 @@ class RocksPage extends ReactiveWidget<RockModel> {
           ...model.filteredRocks.map((rock) => /// Method to create new rows for each Rock
           Row(
             children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                      color: const Color.fromARGB(255, 212, 218, 228),),
-                ),
-                padding: const EdgeInsets.all(40),
-                child: Text(rock.name),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                      color: const Color.fromARGB(255, 212, 218, 228),),
-                  image:  DecorationImage(
-                    image: AssetImage(rock.image),
-                    fit: BoxFit.cover,
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 212, 218, 228),),
                   ),
+                  padding: const EdgeInsets.all(40),
+                  child: Text(rock.name),
                 ),
-                padding: const EdgeInsets.all(50),
               ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                      color: const Color.fromARGB(255, 212, 218, 228),),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 212, 218, 228),),
+                    image:  DecorationImage(
+                      image: AssetImage(rock.image),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(50),
                 ),
-                padding: const EdgeInsets.all(40),
-                child: Text(rock.description),
+              ),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 212, 218, 228),),
+                  ),
+                  padding: const EdgeInsets.all(40),
+                  child: Text(rock.description),
+                ),
               ),
             ],
           ),          ),
