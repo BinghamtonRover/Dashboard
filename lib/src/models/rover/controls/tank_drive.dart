4521380926dcd007db5115389ce3a3aceec4ae95
Gold87 <91761103+Gold872@users.noreply.github.com>
2024-09-18 00:35:31 -0400

    Create button to open ethernet config (#153)
    Co-authored-by: Levi Lesches <levilesches@gmail.com>

--- lib/src/models/rover/controls/tank_drive.dart
+++ lib/src/models/rover/controls/tank_drive.dart
@@ -5,25 +5,46 @@ import "controls.dart";
 
 /// The skid-steer drive controls.
 class DriveControls extends RoverControls {
-	@override
-	OperatingMode get mode => OperatingMode.drive;
+  /// Whether the left shoulder was pressed last tick.
+  bool leftShoulderFlag = false;
 
-	@override
-	List<Message> parseInputs(GamepadState state) => [
-		DriveCommand(setLeft: true, left: state.normalLeftY),
-		DriveCommand(setRight: true, right: -1*state.normalRightY),
-	];
+  /// Whether the right shoulder was pressed last tick.
+  bool rightShoulderFlag = true;
+
+  @override
+  OperatingMode get mode => OperatingMode.drive;
+
+  /// The throttle the rover should be at.
+  double throttle = 0;
+
+  @override
+  void updateState(GamepadState state) {
+    if (!leftShoulderFlag && state.leftShoulder) throttle -= 0.1;
+    leftShoulderFlag = state.leftShoulder;
+    if (!rightShoulderFlag && state.rightShoulder) throttle += 0.1;
+    rightShoulderFlag = state.rightShoulder;
+    throttle = throttle.clamp(0, 1);
+  }
+
+  @override
+  List<Message> parseInputs(GamepadState state) => [
+    DriveCommand(throttle: throttle, setThrottle: true),
+    DriveCommand(setLeft: true, left: state.normalLeftY),
+    DriveCommand(setRight: true, right: -1*state.normalRightY),
+  ];
 
   @override
   List<Message> get onDispose => [
     DriveCommand(setThrottle: true, throttle: 0),
-		DriveCommand(setLeft: true, left: 0),
-		DriveCommand(setRight: true, right: 0),
+    DriveCommand(setLeft: true, left: 0),
+    DriveCommand(setRight: true, right: 0),
   ];
 
-	@override
-	Map<String, String> get buttonMapping => {
-		"Left Throttle": "Left joystick (vertical)",
-		"Right Throttle": "Right joystick (vertical)",
-	};
+  @override
+  Map<String, String> get buttonMapping => {
+    "Left Throttle": "Left joystick (vertical)",
+    "Right Throttle": "Right joystick (vertical)",
+    "Increase throttle": "Right bumper (R1)",
+    "Decrease throttle": "Left bumper (L1)",
+  };
 }
