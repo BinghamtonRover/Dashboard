e04dd050091e35514ed87158df4ef9237c7d070b
Levi Lesches <levilesches@gmail.com>
2024-09-23 20:53:08 -0400

    Use win32 on Windows

--- lib/src/services/gamepad/gamepad.dart
+++ lib/src/services/gamepad/gamepad.dart
@@ -23,7 +23,9 @@ abstract class Gamepad extends Service {
   Gamepad(this.controllerIndex);
 
   /// Returns a functional instance of this class, or a mock on unsupported platforms.
-  factory Gamepad.forPlatform(int index) => DashboardSdlGamepad(index);
+  factory Gamepad.forPlatform(int index) => Platform.isWindows
+    ? Win32Gamepad(index)
+    : DashboardSdlGamepad(index);
 
   /// Gets the current state of the gamepad, or null if it's not connected.
   GamepadState? getState();
