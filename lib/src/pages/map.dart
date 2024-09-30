42ea897a66a8381b5f4dd468676341b46340405e
Gold87 <91761103+Gold872@users.noreply.github.com>
2024-09-30 18:14:28 -0400

    Rearranged the entire page

--- lib/src/pages/map.dart
+++ lib/src/pages/map.dart
@@ -81,115 +81,152 @@ class MapPage extends ReactiveWidget<AutonomyModel> {
         ),
       );
 
-  @override
-  Widget build(BuildContext context, AutonomyModel model) => Stack(
+  /// The controls for creating, placing, and removing markers
+  Widget markerControls(BuildContext context, AutonomyModel model) => Row(
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 48),
-              for (final row in model.grid.reversed)
+          // Controls
+          const SizedBox(width: 4),
+          Text("Place marker: ", style: context.textTheme.titleLarge),
+          const SizedBox(width: 8),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.add),
+            label: const Text("Add Marker"),
+            onPressed: () => placeMarker(context, model),
+          ),
+          const SizedBox(width: 8),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.location_on),
+            label: const Text("Drop marker here"),
+            onPressed: model.placeMarkerOnRover,
+          ),
+          const SizedBox(width: 8),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.clear),
+            label: const Text("Clear all"),
+            onPressed: model.clearMarkers,
+          ),
+          const Spacer(),
+          Text("Zoom: ", style: context.textTheme.titleLarge),
+          Expanded(
+            flex: 2,
+            child: Slider(
+              value: model.gridSize.toDouble(),
+              min: 1,
+              max: 41,
+              divisions: 20,
+              label: "${model.gridSize}x${model.gridSize}",
+              onChanged: (value) => model.zoom(value.toInt()),
+            ),
+          ),
+        ],
+      );
+
+  @override
+  Widget build(BuildContext context, AutonomyModel model) => Column(
+        children: [
+          MapPageHeader(model: model, index: index),
+          const SizedBox(height: 24),
+          Expanded(
+            child: Row(
+              children: [
+                if (!model.isPlayingBadApple) ...[
+                  const SizedBox(width: 16),
+                  const MapLegend(),
+                  const SizedBox(width: 16),
+                ],
                 Expanded(
-                  child: Row(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      for (final cell in row) createCell(model, cell),
+                      for (final row in model.grid.reversed)
+                        Expanded(
+                          child: Row(
+                            children: [
+                              for (final cell in row) createCell(model, cell),
+                            ],
+                          ),
+                        ),
+                      if (!model.isPlayingBadApple) markerControls(context, model),
+                      const SizedBox(height: 4),
+                      AutonomyCommandEditor(model),
+                      const SizedBox(height: 12),
                     ],
                   ),
                 ),
-              const SizedBox(height: 4),
-              if (!model.isPlayingBadApple)
-                Row(
-                  children: [
-                    // Legend
-                    const SizedBox(width: 4),
-                    Text("Legend:", style: context.textTheme.titleLarge),
-                    const SizedBox(width: 8),
-                    Container(width: 24, height: 24, color: Colors.blue),
-                    const SizedBox(width: 4),
-                    Text("Rover", style: context.textTheme.titleMedium),
-                    const SizedBox(width: 24),
-                    Container(width: 24, height: 24, color: Colors.green),
-                    const SizedBox(width: 4),
-                    Text("Destination", style: context.textTheme.titleMedium),
-                    const SizedBox(width: 24),
-                    Container(width: 24, height: 24, color: Colors.black),
-                    const SizedBox(width: 4),
-                    Text("Obstacle", style: context.textTheme.titleMedium),
-                    const SizedBox(width: 24),
-                    Container(width: 24, height: 24, color: Colors.blueGrey),
-                    const SizedBox(width: 4),
-                    Text("Path", style: context.textTheme.titleMedium),
-                    const SizedBox(width: 24),
-                    Container(width: 24, height: 24, color: Colors.red),
-                    const SizedBox(width: 4),
-                    Text("Marker", style: context.textTheme.titleMedium),
-                    const Spacer(),
-                    Text("Zoom: ", style: context.textTheme.titleLarge),
-                    Expanded(
-                      flex: 2,
-                      child: Slider(
-                        value: model.gridSize.toDouble(),
-                        min: 1,
-                        max: 41,
-                        divisions: 20,
-                        label: "${model.gridSize}x${model.gridSize}",
-                        onChanged: (value) => model.zoom(value.toInt()),
-                      ),
-                    ),
-                  ],
-                ),
-              if (!model.isPlayingBadApple)
-                Row(
-                  children: [
-                    // Controls
-                    const SizedBox(width: 4),
-                    Text("Place marker: ", style: context.textTheme.titleLarge),
-                    const SizedBox(width: 8),
-                    ElevatedButton.icon(
-                      icon: const Icon(Icons.add),
-                      label: const Text("Add Marker"),
-                      onPressed: () => placeMarker(context, model),
-                    ),
-                    const SizedBox(width: 8),
-                    ElevatedButton.icon(
-                      icon: const Icon(Icons.location_on),
-                      label: const Text("Drop marker here"),
-                      onPressed: model.placeMarkerOnRover,
-                    ),
-                    const SizedBox(width: 8),
-                    ElevatedButton.icon(
-                        icon: const Icon(Icons.clear), label: const Text("Clear all"), onPressed: model.clearMarkers),
-                    const Spacer(),
-                  ],
-                ),
-              const SizedBox(height: 8),
-              AutonomyCommandEditor(model),
-              const VerticalDivider(),
-              const SizedBox(height: 4),
-            ],
-          ),
-          Container(
-            color: context.colorScheme.surface,
-            height: 50,
-            child: Row(
-              children: [
-                // The header at the top
-                const SizedBox(width: 8),
-                Text("Map", style: context.textTheme.headlineMedium),
-                if (models.settings.easterEggs.badApple)
-                  IconButton(
-                    iconSize: 48,
-                    icon: CircleAvatar(
-                      backgroundImage: const AssetImage("assets/bad_apple_thumbnail.webp"),
-                      child: model.isPlayingBadApple ? const Icon(Icons.block, color: Colors.red, size: 36) : null,
-                    ),
-                    onPressed: model.isPlayingBadApple ? model.stopBadApple : model.startBadApple,
-                  ),
-                const Spacer(),
-                ViewsSelector(index: index),
+                const SizedBox(width: 16),
               ],
             ),
           ),
         ],
+      );
+}
+
+/// Displays the legend for what each cell color represents on the map
+class MapLegend extends StatelessWidget {
+  /// Const constructor for the map legend
+  const MapLegend({super.key});
+
+  @override
+  Widget build(BuildContext context) => Column(
+        children: [
+          const SizedBox(height: 4),
+          Text("Legend:", style: context.textTheme.titleLarge),
+          const SizedBox(height: 8),
+          Container(width: 24, height: 24, color: Colors.blue),
+          const SizedBox(height: 4),
+          Text("Rover", style: context.textTheme.titleMedium),
+          const SizedBox(height: 24),
+          Container(width: 24, height: 24, color: Colors.green),
+          const SizedBox(height: 4),
+          Text("Destination", style: context.textTheme.titleMedium),
+          const SizedBox(height: 24),
+          Container(width: 24, height: 24, color: Colors.black),
+          const SizedBox(height: 4),
+          Text("Obstacle", style: context.textTheme.titleMedium),
+          const SizedBox(height: 24),
+          Container(width: 24, height: 24, color: Colors.blueGrey),
+          const SizedBox(height: 4),
+          Text("Path", style: context.textTheme.titleMedium),
+          const SizedBox(height: 24),
+          Container(width: 24, height: 24, color: Colors.red),
+          const SizedBox(height: 4),
+          Text("Marker", style: context.textTheme.titleMedium),
+        ],
+      );
+}
+
+/// The header to appear at the top of the map page, displays a button to play bad apple...
+class MapPageHeader extends StatelessWidget {
+  /// The model to control the status of bad apple...
+  final AutonomyModel model;
+
+  /// Index of the view the header is in
+  final int index;
+
+  /// Const constructor for the map page header
+  const MapPageHeader({required this.index, required this.model, super.key});
+
+  @override
+  Widget build(BuildContext context) => Container(
+        color: context.colorScheme.surface,
+        height: 50,
+        child: Row(
+          children: [
+            // The header at the top
+            const SizedBox(width: 8),
+            Text("Map", style: context.textTheme.headlineMedium),
+            if (models.settings.easterEggs.badApple)
+              IconButton(
+                iconSize: 48,
+                icon: CircleAvatar(
+                  backgroundImage: const AssetImage("assets/bad_apple_thumbnail.webp"),
+                  child: model.isPlayingBadApple ? const Icon(Icons.block, color: Colors.red, size: 36) : null,
+                ),
+                onPressed: model.isPlayingBadApple ? model.stopBadApple : model.startBadApple,
+              ),
+            const Spacer(),
+            ViewsSelector(index: index),
+          ],
+        ),
       );
 }
