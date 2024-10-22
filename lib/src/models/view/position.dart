a835e7d69fea46fea67f26a771b30cb4945ad2ec
Gold87 <91761103+Gold872@users.noreply.github.com>
2024-10-22 19:15:02 -0400

    Fix drive page

--- lib/src/models/view/position.dart
+++ lib/src/models/view/position.dart
@@ -60,28 +60,34 @@ class PositionModel with ChangeNotifier {
       wheelColors[i] = Colors.black;
     }
     final leftAvg = (wheelsRPM[0] + wheelsRPM[1] + wheelsRPM[2]) / 3;
-    var maxDistance = 0.0; 
-    var index = 0;
-    for(var i = 0; i < 3; i++){
-      if((wheelsRPM[i] - leftAvg).abs() > 100){ // Threshold is 100 difference from mean
-        if((wheelsRPM[i] - leftAvg).abs() > maxDistance){
+    final rightAvg = (wheelsRPM[3] + wheelsRPM[4] + wheelsRPM[5]) / 3;
+    var maxDistance = 0.0;
+    var index = -1;
+    for (var i = 0; i < 3; i++) {
+      if ((wheelsRPM[i] - leftAvg).abs() > 100) {
+        // Threshold is 100 difference from mean
+        if ((wheelsRPM[i] - leftAvg).abs() > maxDistance) {
           index = i;
           maxDistance = (wheelsRPM[i] - leftAvg).abs();
         }
       }
     }
-    wheelColors[index] = Colors.yellow;
+    if (index != -1) {
+      wheelColors[index] = Colors.yellow;
+    }
+    index = -1;
     maxDistance = 0.0;
-    final rightAvg = (wheelsRPM[3] + wheelsRPM[4] + wheelsRPM[5]) / 3;
-    for(var i = 3; i < 6; i++){
-      if((wheelsRPM[i] - rightAvg).abs() > 100){
-        if((wheelsRPM[i] - rightAvg).abs() > maxDistance){
+    for (var i = 3; i < 6; i++) {
+      if ((wheelsRPM[i] - rightAvg).abs() > 100) {
+        if ((wheelsRPM[i] - rightAvg).abs() > maxDistance) {
           index = i;
           maxDistance = (wheelsRPM[i] - rightAvg).abs();
         }
-      } 
+      }
     }
-    wheelColors[index] = Colors.yellow;
+    if (index != -1) {
+      wheelColors[index] = Colors.yellow;
+    }
   }
 
   @override
