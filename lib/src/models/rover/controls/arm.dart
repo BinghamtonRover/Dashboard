14066ef313ec389a8ec52dd7701009696bfd4101
Gold87 <91761103+Gold872@users.noreply.github.com>
2024-11-19 01:53:47 -0500

    Added Controllers Page (#177)
    ![image](https://github.com/user-attachments/assets/3a1fc8a9-b3d6-45e3-8ea6-16db9e9a2e5d)
    
    Useful to debugging issues with the controller itself or the Dashboard's code for reading it
    
    ---------
    
    Co-authored-by: Levi Lesches <levilesches@gmail.com>

--- lib/src/models/rover/controls/arm.dart
+++ lib/src/models/rover/controls/arm.dart
@@ -38,21 +38,21 @@ class ArmControls extends RoverControls {
 	@override
 	List<Message> parseInputs(GamepadState state) => [
     // Manual control
-    if (state.normalRightX.abs() > state.normalRightY.abs() && state.normalRightX != 0) 
+    if (state.normalRightX.abs() > state.normalRightJoystickY.abs() && state.normalRightX != 0) 
       ArmCommand(swivel: MotorCommand(moveRadians: state.normalRightX * settings.swivel)),
-    if (state.normalRightY.abs() > state.normalRightX.abs() && state.normalRightY != 0) 
-      ArmCommand(shoulder: MotorCommand(moveRadians: state.normalRightY * settings.shoulder)),
+    if (state.normalRightJoystickY.abs() > state.normalRightX.abs() && state.normalRightJoystickY != 0) 
+      ArmCommand(shoulder: MotorCommand(moveRadians: state.normalRightJoystickY * settings.shoulder)),
     if (state.normalLeftY != 0) ArmCommand(elbow: MotorCommand(moveRadians: state.normalLeftY * settings.elbow)),
     // The bumpers should be pseudo-IK: Move the shoulder and elbow in sync. 
-    if (state.normalShoulder != 0) ArmCommand(
-      shoulder: MotorCommand(moveRadians: state.normalShoulder * settings.shoulder * -1),
-      elbow: MotorCommand(moveRadians: state.normalShoulder * settings.elbow),
+    if (state.normalShoulders != 0) ArmCommand(
+      shoulder: MotorCommand(moveRadians: state.normalShoulders * settings.shoulder * -1),
+      elbow: MotorCommand(moveRadians: state.normalShoulders * settings.elbow),
     ),
 
 		// Gripper
 		if (state.normalDpadY != 0) GripperCommand(lift: MotorCommand(moveRadians: state.normalDpadY * settings.lift)),
 		if (state.normalDpadX != 0) GripperCommand(rotate: MotorCommand(moveRadians: state.normalDpadX * settings.rotate)),
-		if (state.normalTrigger != 0) GripperCommand(pinch: MotorCommand(moveRadians: state.normalTrigger * settings.pinch)),
+		if (state.normalTriggers != 0) GripperCommand(pinch: MotorCommand(moveRadians: state.normalTriggers * settings.pinch)),
 
 		// Custom actions
 		if (state.buttonA && !isAPressed) () { isAPressed = true; return GripperCommand(open: true); }(),
