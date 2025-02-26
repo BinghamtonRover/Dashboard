5eaea73a285cd2b2ad30da51162e9162973f18f0
vadileep <120128895+vadileep@users.noreply.github.com>
2025-02-25 23:21:18 -0500

    Failedupdates

--- Flutter Tutorial/test/widget_test.dart
+++ Flutter Tutorial/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutter_counter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(AppBar());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
