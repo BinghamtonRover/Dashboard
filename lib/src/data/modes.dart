bdcfca2c87b550812c673f686f733af6e0991c17
Levi Lesches <levilesches@gmail.com>
2025-03-21 00:38:24 -0400

    Re-organized the footer

--- lib/src/data/modes.dart
+++ lib/src/data/modes.dart
@@ -1,40 +1,40 @@
-/// A mode for operating the rover. 
-/// 
-/// The operator can switch between modes which will: 
+/// A mode for operating the rover.
+///
+/// The operator can switch between modes which will:
 /// - change the controller inputs to match the current mode
 /// - change the on-screen UI to provide information useful in this context
 /// - highlight the relevant metrics
-enum OperatingMode { 
+enum OperatingMode {
 	/// No controls. Allows the user to "disable" a gamepad.
 	none("None"),
 
-	/// Skid-steer drive controls. 
-	/// 
+  /// Modern drive controls.
+  ///
+  /// Focus on driving intuitively with simple controls
+  modernDrive("Drive"),
+
+	/// Skid-steer drive controls.
+	///
 	/// Focus on helping the user drive the rover with as much manual control.
 	drive("Tank drive"),
 
-  /// Modern drive controls.
-  /// 
-  /// Focus on driving intuitively with simple controls 
-  modernDrive("Drive"),
-
-	/// Camera mode.
-	/// 
-	/// The on-board cameras are on servo mounts. This mode controls those mounts.
-	cameras("Cameras"),
-
-	/// Science mode. 
-	/// 
+	/// Science mode.
+	///
 	/// Focus cameras on the science chamber and allow the user to collect samples and data.
-	science("Science"), 
+	science("Science"),
 
 	/// Arm mode.
-	/// 
+	///
 	/// Focus on helping the user manipulate the arm.
-	arm("Arm");
+	arm("Arm"),
+
+	/// Camera mode.
+	///
+	/// The on-board cameras are on servo mounts. This mode controls those mounts.
+	cameras("Cameras");
 
 	/// The name of this mode.
-	/// 
+	///
 	/// Use this instead of [EnumName.name] so we can use a longer name.
 	final String name;
 
