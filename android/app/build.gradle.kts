a1b949cdececd39c767dace40c1390b30ba67a61
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-05-11 23:46:56 -0400

    Fix android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "edu.binghamton.rover.dashboard"
+    namespace = "edu.binghamton.rover.rover_dashboard"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "edu.binghamton.rover.dashboard"
+        applicationId = "edu.binghamton.rover.rover_dashboard"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
