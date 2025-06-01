512c09d6e026240249de23bfed8fa1428a96e7cc
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-06-01 19:45:05 -0400

    Change gripper icon to compress

--- lib/src/data/metrics/gripper.dart
+++ lib/src/data/metrics/gripper.dart
@@ -1,39 +1,43 @@
+import "package:flutter/material.dart";
 import "package:rover_dashboard/data.dart";
 
 /// Metrics about the gripper on the HREI subsystem.
 class GripperMetrics extends Metrics<GripperData> {
-	/// Metrics about the gripper.
-	GripperMetrics() : super(GripperData());
+  /// Metrics about the gripper.
+  GripperMetrics() : super(GripperData());
 
-	@override
-	String name = "Gripper";
+  @override
+  String name = "Gripper";
 
   @override
   Version supportedVersion = Version(major: 1);
 
+  @override
+  IconData icon = Icons.compress;
+
 	/// Returns a description of a [MotorData].
-	List<MetricLine> getMotorData(MotorData motor) => [
-		MetricLine("  Is moving? ${motor.isMoving.displayName}", severity: motor.isMoving.toBool() ? Severity.info : null),
-		MetricLine("  Limit? ${motor.isLimitSwitchPressed.displayName}", severity: motor.isLimitSwitchPressed.toBool() ? Severity.warning : null),
-		MetricLine("  Direction: ${motor.direction.humanName}"),
-		MetricLine("  Steps: ${motor.currentStep} --> ${motor.targetStep}"),
-		MetricLine("  Angle: ${motor.currentAngle.toDegrees() % 360} degrees"),
-	];
+  List<MetricLine> getMotorData(MotorData motor) => [
+    MetricLine("  Is moving? ${motor.isMoving.displayName}", severity: motor.isMoving.toBool() ? Severity.info : null),
+    MetricLine("  Limit? ${motor.isLimitSwitchPressed.displayName}", severity: motor.isLimitSwitchPressed.toBool() ? Severity.warning : null),
+    MetricLine("  Direction: ${motor.direction.humanName}"),
+    MetricLine("  Steps: ${motor.currentStep} --> ${motor.targetStep}"),
+    MetricLine("  Angle: ${motor.currentAngle.toDegrees() % 360} degrees"),
+  ];
 
 	@override
-	List<MetricLine> get allMetrics => [
+  List<MetricLine> get allMetrics => [
     MetricLine("Camera Angle: ${data.servoAngle} degrees"),
     MetricLine("Laser: ${data.laserState.displayName}", severity: data.laserState.toBool() ? Severity.critical : null),
     MetricLine("------------------------------",),
     MetricLine("Lift:"),
-		...getMotorData(data.lift,),
-		MetricLine("------------------------------",),
+    ...getMotorData(data.lift,),
+    MetricLine("------------------------------",),
     MetricLine("Rotate"),
-		...getMotorData(data.rotate),
-		MetricLine("------------------------------",),
+    ...getMotorData(data.rotate),
+    MetricLine("------------------------------",),
     MetricLine("Pinch:"),
-		...getMotorData(data.pinch),
-	];
+    ...getMotorData(data.pinch),
+  ];
 
   @override
   Version parseVersion(GripperData message) => message.version;
