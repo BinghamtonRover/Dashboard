ff58b4170e3179b81542180b5e23f455db8d1948
Gold87 <91761103+Gold872@users.noreply.github.com>
2026-05-28 22:23:03 -0600

    Reformat drive page

--- lib/src/pages/drive.dart
+++ lib/src/pages/drive.dart
@@ -10,16 +10,14 @@ import "package:rover_dashboard/widgets.dart";
 class _BarChart extends StatelessWidget {
   final List<double> values;
 
-  const _BarChart({
-    required this.values, 
-  });
+  const _BarChart({required this.values});
 
   @override
   Widget build(BuildContext context) => BarChart(
     BarChartData(
       barTouchData: BarTouchData(
         touchTooltipData: BarTouchTooltipData(
-          fitInsideVertically: true, 
+          fitInsideVertically: true,
           fitInsideHorizontally: true,
           getTooltipItem: (group, groupIndex, rod, rodIndex) {
             final color = rod.gradient?.colors.first ?? rod.color;
@@ -45,9 +43,7 @@ class _BarChart extends StatelessWidget {
         ),
       ),
       borderData: FlBorderData(show: true),
-      gridData: const FlGridData(
-        drawVerticalLine: false,
-      ),
+      gridData: const FlGridData(drawVerticalLine: false),
       barGroups: barGroups,
       extraLinesData: ExtraLinesData(horizontalLines: [HorizontalLine(y: 0)]),
     ),
@@ -74,7 +70,7 @@ class _BarChart extends StatelessWidget {
       child: Text(text, style: style),
     );
   }
-    
+
   List<BarChartGroupData> get barGroups => [
     BarChartGroupData(
       x: 0,
@@ -104,79 +100,71 @@ class _BarChart extends StatelessWidget {
     ),
   ];
 }
-class _WheelRPM extends StatelessWidget{
+
+class _WheelRPM extends StatelessWidget {
   final double rpm;
   final Color color;
 
-  const _WheelRPM({
-    required this.rpm,
-    required this.color, 
-  });
+  const _WheelRPM({required this.rpm, required this.color});
 
   @override
   Widget build(BuildContext context) => Expanded(
     child: Container(
       alignment: Alignment.center,
-      width: 50, 
+      width: 50,
       height: 70,
       color: color,
       child: Text(
         rpm.toStringAsFixed(1),
-        style: const TextStyle(
-          color: Colors.red,
-          fontWeight: FontWeight.bold,
-        ),
-      ), 
+        style: const TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+      ),
     ),
   );
 }
-class _Wheels extends StatelessWidget{
+
+class _Wheels extends StatelessWidget {
   final List<double> wheels;
   final List<Color> colors;
 
-  const _Wheels({
-    required this.colors,
-    required this.wheels, 
-  });
+  const _Wheels({required this.colors, required this.wheels});
 
   @override
   Widget build(BuildContext context) => Column(
     children: [
       const Text(
-        "Wheel RPMs", 
+        "Wheel RPMs",
         textAlign: TextAlign.center,
-        style: TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      Expanded(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _WheelRPM(rpm: wheels[0], color: colors[0]),
+                const SizedBox(height: 8),
+                _WheelRPM(rpm: wheels[1], color: colors[1]),
+                const SizedBox(height: 8),
+                _WheelRPM(rpm: wheels[2], color: colors[2]),
+                const SizedBox(height: 8),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _WheelRPM(rpm: wheels[3], color: colors[3]),
+                const SizedBox(height: 8),
+                _WheelRPM(rpm: wheels[4], color: colors[4]),
+                const SizedBox(height: 8),
+                _WheelRPM(rpm: wheels[5], color: colors[5]),
+                const SizedBox(height: 8),
+              ],
+            ),
+          ],
         ),
       ),
-      Expanded(child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _WheelRPM(rpm: wheels[0], color: colors[0]),
-              const SizedBox(height: 8,),
-              _WheelRPM(rpm: wheels[1], color: colors[1]),
-              const SizedBox(height: 8,),
-              _WheelRPM(rpm: wheels[2], color: colors[2]),
-              const SizedBox(height: 8,),
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _WheelRPM(rpm: wheels[3], color: colors[3]),
-              const SizedBox(height: 8,),
-              _WheelRPM(rpm: wheels[4], color: colors[4]),
-              const SizedBox(height: 8,),
-              _WheelRPM(rpm: wheels[5], color: colors[5]),
-              const SizedBox(height: 8,),
-            ],
-          ),
-        ],
-      ),),
     ],
   );
 }
@@ -185,14 +173,15 @@ class _Wheels extends StatelessWidget{
 class DrivePage extends ReactiveWidget<PositionModel> {
   /// The index of this view.
   final int index;
+
   /// A const constructor.
   const DrivePage({required this.index});
-  
+
   @override
   PositionModel createModel() => PositionModel();
 
   @override
-	Widget build(BuildContext context, PositionModel model) => Column(
+  Widget build(BuildContext context, PositionModel model) => Column(
     crossAxisAlignment: CrossAxisAlignment.stretch,
     children: [
       PageHeader(
@@ -210,35 +199,36 @@ class DrivePage extends ReactiveWidget<PositionModel> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Expanded(
-              child: Column(children: [
-                const Text(
-                  "Front View", 
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+              child: Column(
+                children: [
+                  const Text(
+                    "Front View",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                ),
-                Expanded(child: Transform.rotate(
-		     
-		     angle: model.position.roll * (pi/180),
-                     child: Image.asset("assets/rover_front.png"), 
-                ),),
-              ],),
+                  Expanded(
+                    child: Transform.rotate(
+                      angle: model.position.roll * (pi / 180),
+                      child: Image.asset("assets/rover_front.png"),
+                    ),
+                  ),
+                ],
+              ),
             ),
             Expanded(
-              child: Column(children: [
-                const Text(
-                  "Side View", 
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+              child: Column(
+                children: [
+                  const Text(
+                    "Side View",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                ),
-                Expanded(child: Transform.rotate(
-                     angle: model.position.pitch * (pi/180),
-                     child: Image.asset("assets/rover_side.png", scale: 0.5),             
-                ),),
-              ],),
+                  Expanded(
+                    child: Transform.rotate(
+                      angle: model.position.pitch * (pi / 180),
+                      child: Image.asset("assets/rover_side.png", scale: 0.5),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -247,11 +237,18 @@ class DrivePage extends ReactiveWidget<PositionModel> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Expanded(child: _Wheels(wheels: model.wheelsRPM, colors: model.wheelColors)),
-            Expanded(child: _BarChart(values: [model.leftWheels, -model.rightWheels])),
+            Expanded(
+              child: _Wheels(
+                wheels: model.wheelsRPM,
+                colors: model.wheelColors,
+              ),
+            ),
+            Expanded(
+              child: _BarChart(values: [model.leftWheels, -model.rightWheels]),
+            ),
           ],
         ),
       ),
     ],
-  ); 
+  );
 }
